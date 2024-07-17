377916e826892177b7a5e08c00756d4e9f5cfec2
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-07-17 18:25:29 -0500

    Actualizar gc-invitar.js

--- plugins/gc-invitar.js
+++ plugins/gc-invitar.js
@@ -4,9 +4,8 @@ if (text.includes('+')) throw  `🚩 Ingrese el número todo junto sin el *+*`
 if (isNaN(text)) throw '🍟 Ingrese sólo números más su código de país sin espacios'
 let group = m.chat
 let link = 'https://chat.whatsapp.com/' + await conn.groupInviteCode(group)
-
-      await conn.reply(text+'@s.whatsapp.net', `🍟 *INVITACIÓN A GRUPO*\n\nUn usuario te invitó a unirte a este grupo \n\n${link}`, m, {mentions: [m.sender]})
-        m.reply(`🍟 Se envió un enlace de invitación al usuario.`) 
+await conn.reply(text+'@s.whatsapp.net', `🍟 *INVITACIÓN A GRUPO*\n\nUn usuario te invitó a unirte a este grupo \n\n${link}`, m, {mentions: [m.sender]})
+m.reply(`🍟 Se envió un enlace de invitación al usuario.`) 
 
 }
 handler.help = ['invite *<numero>*']
