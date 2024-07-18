58644670931d63733af7afa2b05200bf9f9d89dc
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-07-17 21:21:25 -0500

    Actualizar mods-join.js

--- plugins/mods-join.js
+++ plugins/mods-join.js
@@ -2,11 +2,11 @@ let linkRegex = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})( [0-9]{1,3})?/i
 
 let handler = async (m, { conn, text, isOwner, usedPrefix, command }) => {
 
-if (!text) return m.reply(`🍭 Ingresa el enlace del Grupo.`)
+if (!text) return m.reply(`🍟 Ingresa el enlace del Grupo.`)
 let [_, code] = text.match(linkRegex) || []
-if (!code) return m.reply('Enlace invalido.')
+if (!code) return m.reply('🐢 Enlace invalido.')
 let res = await conn.groupAcceptInvite(code)
-m.reply(`🍭 Me uní correctamente al Grupo`)
+m.reply(`🚩 Me uní correctamente al Grupo`)
 }
 handler.help = ['join <link>']
 handler.tags = ['mods']
