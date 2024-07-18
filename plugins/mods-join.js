b5f4fd3c4acf12f05e7b1d601bfd11add1b9cb62
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-07-17 21:13:17 -0500

    Actualizar mods-join.js

--- plugins/mods-join.js
+++ plugins/mods-join.js
@@ -1,16 +1,26 @@
+import db from '../lib/database.js'
 let linkRegex = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})( [0-9]{1,3})?/i
 
 let handler = async (m, { conn, text, isOwner, usedPrefix, command }) => {
 
-if (!text) return conn.reply(m.chat, `🍟 *Ingresa el enlace del Grupo.*\n\nEjemplo: ${usedPrefix + command} ${gp1}`, m, rcanal)
-let [_, code] = text.match(linkRegex) || []
-if (!code) return conn.reply(m.chat, 'Enlace invalido.', m, rcanal)
-let res = await conn.groupAcceptInvite(code)
-conn.reply(m.chat, '✅️ Me uní correctamente al Grupo', m, rcanal)
+        if (!text) return m.reply(`🚩 Ingresa el enlace del Grupo.`)
+    let [_, code, expired] = text.match(linkRegex) || []
+    if (!code) return m.reply('🚩 Enlace invalido.')
+    let res = await conn.groupAcceptInvite(code)
+    expired = Math.floor(Math.min(999, Math.max(1, isOwner ? isNumber(expired) ? parseInt(expired) : 0 : 3)))
+    m.reply(`🚩 Me uní correctamente al Grupo *${res}${expired ? `* Durante *${expired}* días.` : ''}`)
+    let chats = global.db.data.chats[res]
+    if (!chats) chats = global.db.data.chats[res] = {}
+    if (expired) chats.expired = +new Date() + expired * 1000 * 60 * 60 * 24
+    let pp = 'https://telegra.ph/file/4fa3f65b6698517cd8dcf.mp4'
+   await conn.sendMessage(res, { video: { url: pp }, gifPlayback: true, caption: 'Ya llego el mejor Bot de todo WhatsApp.', mentions: [m.sender] }, { quoted: fkontak })
 }
-handler.help = ['join <link>']
+handler.help = ['join *<link> <días>*']
 handler.tags = ['mods']
+
 handler.command = ['join', 'entrar'] 
 handler.mods = true
 
 export default handler
+
+const isNumber = (x) => (x = parseInt(x), typeof x === 'number' && !isNaN(x))
