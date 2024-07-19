2f2dd8fad913b2b166b96141acad5fe8dd55eb99
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-07-18 23:04:51 -0500

    Update _welcome.js

--- plugins/_welcome.js
+++ plugins/_welcome.js
@@ -12,15 +12,15 @@ export async function before(m, {conn, participants, groupMetadata}) {
   if (chat.welcome && m.messageStubType == 27) {
     let welcome = `┌─★ *Luffy Bot - MD* \n│「 Bienvenido 」\n└┬★ 「 @${m.messageStubParameters[0].split`@`[0]} 」\n   │✑  Bienvenido a\n   │✑  ${groupMetadata.subject}\n   └───────────────┈ ⳹`
 
-await conn.sendLuffy(m.chat, packname, textbot, welcome, img, img, redes, fkontak)
+await conn.sendLuffy(m.chat, packname, textbot, welcome, img, img, redes)
   }
 
   if (chat.welcome && m.messageStubType == 28) {
     let bye = `┌─★ *Luffy Bot - MD* \n│「 ADIOS 👋 」\n└┬★ 「 @${m.messageStubParameters[0].split`@`[0]} 」\n   │✑  Se fue\n   │✑ Jamás te quisimos aquí\n   └───────────────┈ ⳹`
-await conn.sendLuffy(m.chat, packname, textbot, bye, img2, img2, redes fkontak)
+await conn.sendLuffy(m.chat, packname, textbot, bye, img2, img2, redes)
   }
 
   if (chat.welcome && m.messageStubType == 32) {
     let kick = `┌─★ *Luffy Bot -MD* \n│「 ADIOS 👋 」\n└┬★ 「 @${m.messageStubParameters[0].split`@`[0]} 」\n   │✑  Se fue\n   │✑ Jamás te quisimos aquí\n   └───────────────┈ ⳹`
-await conn.sendLuffy(m.chat, packname, textbot, kick, img2, img2, redes, fkontak)
+await conn.sendLuffy(m.chat, packname, textbot, kick, img2, img2, redes)
 }}
