135c802b27144443f4ef6c1bfc9e7e7cc453f8bf
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-07-18 23:07:03 -0500

    Update _welcome.js

--- plugins/_welcome.js
+++ plugins/_welcome.js
@@ -3,10 +3,8 @@ import fetch from 'node-fetch'
 
 export async function before(m, {conn, participants, groupMetadata}) {
   if (!m.messageStubType || !m.isGroup) return !0;
-  //let pp = await conn.profilePictureUrl(m.messageStubParameters[0], 'image').catch(_ => imagen1)
-  //let img = await (await fetch(`${pp}`)).buffer()
-  let img = 'https://telegra.ph/file/78a00ba0041d163e8fb71.jpg'
-  let img2 = 'https://telegra.ph/file/4ee6e6913de4638ec9dcd.jpg'
+  let img = imagen1
+  let img2 = imagen2
   let chat = global.db.data.chats[m.chat]
 
   if (chat.welcome && m.messageStubType == 27) {
