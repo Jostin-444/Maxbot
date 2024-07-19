0f9b2b0f81f4d1f10ef7ef56e323bc6df1cd5be1
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-07-18 23:00:51 -0500

    Update _welcome.js

--- plugins/_welcome.js
+++ plugins/_welcome.js
@@ -5,21 +5,22 @@ export async function before(m, {conn, participants, groupMetadata}) {
   if (!m.messageStubType || !m.isGroup) return !0;
   //let pp = await conn.profilePictureUrl(m.messageStubParameters[0], 'image').catch(_ => imagen1)
   //let img = await (await fetch(`${pp}`)).buffer()
-  let img = imagen1
+  let img = 'https://telegra.ph/file/78a00ba0041d163e8fb71.jpg'
+  let img2 = ''
   let chat = global.db.data.chats[m.chat]
 
   if (chat.welcome && m.messageStubType == 27) {
     let welcome = `┌─★ *Luffy Bot - MD* \n│「 Bienvenido 」\n└┬★ 「 @${m.messageStubParameters[0].split`@`[0]} 」\n   │✑  Bienvenido a\n   │✑  ${groupMetadata.subject}\n   └───────────────┈ ⳹`
 
-await conn.sendLuffy(m.chat, packname, textbot, welcome, img, img, redes)
+await conn.sendLuffy(m.chat, packname, textbot, welcome, img, img, redes, fkontak)
   }
 
   if (chat.welcome && m.messageStubType == 28) {
     let bye = `┌─★ *Luffy Bot - MD* \n│「 ADIOS 👋 」\n└┬★ 「 @${m.messageStubParameters[0].split`@`[0]} 」\n   │✑  Se fue\n   │✑ Jamás te quisimos aquí\n   └───────────────┈ ⳹`
-await conn.sendLuffy(m.chat, packname, textbot, bye, img, img, redes)
+await conn.sendLuffy(m.chat, packname, textbot, bye, img2, img2, redes fkontak)
   }
 
   if (chat.welcome && m.messageStubType == 32) {
     let kick = `┌─★ *Luffy Bot -MD* \n│「 ADIOS 👋 」\n└┬★ 「 @${m.messageStubParameters[0].split`@`[0]} 」\n   │✑  Se fue\n   │✑ Jamás te quisimos aquí\n   └───────────────┈ ⳹`
-await conn.sendLuffy(m.chat, packname, textbot, kick, img, img, redes)
+await conn.sendLuffy(m.chat, packname, textbot, kick, img2, img2, redes, fkontak)
 }}
