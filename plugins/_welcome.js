e44b10fa5dd227781a083e6703a71592559a3cfc
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-07-20 13:38:29 -0500

    Actualizar _welcome.js

--- plugins/_welcome.js
+++ plugins/_welcome.js
@@ -4,7 +4,7 @@ import fetch from 'node-fetch'
 export async function before(m, {conn, participants, groupMetadata}) {
   if (!m.messageStubType || !m.isGroup) return !0;  
   let img = fs.readFileSync('./media/database/Welcome.js')
-  let img2 = fs.readFileSync('./media/database/Bye.js');
+  let img2 = fs.readFileSync('./media/database/Bye.js')
   let chat = global.db.data.chats[m.chat]
 
   if (chat.welcome && m.messageStubType == 27) {
