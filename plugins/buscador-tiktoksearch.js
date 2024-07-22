3771871e4f456a2cbd6c8422c1462bc3ec7f69ba
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-07-21 23:43:45 -0500

    Actualizar buscador-tiktoksearch.js

--- plugins/buscador-tiktoksearch.js
+++ plugins/buscador-tiktoksearch.js
@@ -1,35 +1,35 @@
-import axios from 'axios';
-const {proto, generateWAMessageFromContent, prepareWAMessageMedia, generateWAMessageContent, getDevice} = (await import("@whiskeysockets/baileys")).default;
+import axios from 'axios'
+const {proto, generateWAMessageFromContent, prepareWAMessageMedia, generateWAMessageContent, getDevice} = (await import("@whiskeysockets/baileys")).default
 
 let handler = async (message, { conn, text, usedPrefix, command }) => {
 
-  if (!text) return conn.reply(message.chat, '🍟 *¿Que quieres buscar en tiktok?*', message, rcanal);
+  if (!text) return conn.reply(message.chat, '🍟 *¿Que quieres buscar en tiktok?*', message, rcanal)
 
   async function createVideoMessage(url) {
-    const { videoMessage } = await generateWAMessageContent({ video: { url } }, { upload: conn.waUploadToServer });
-    return videoMessage;
+    const { videoMessage } = await generateWAMessageContent({ video: { url } }, { upload: conn.waUploadToServer })
+    return videoMessage
   }
 
   async function shuffleArray(array) {
     for (let i = array.length - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * (i + 1));
-      [array[i], array[j]] = [array[j], array[i]];
+      [array[i], array[j]] = [array[j], array[i]]
     }
   }
 
   try {
-  await message.react(rwait);
+  await message.react(rwait)
   conn.reply(message.chat, '🚩 *Descargando Su Video...*', message, {
    contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, showAdAttribution: true,
    title: packname,
    body: wm,
    previewType: 0, thumbnail: icons,
-   sourceUrl: channel }}});
-    let results = [];
-    let { data: response } = await axios.get('https://apis-starlights-team.koyeb.app/starlight/tiktoksearch?text=' + text);
-    let searchResults = response.data;
+   sourceUrl: channel }}})
+    let results = []
+    let { data: response } = await axios.get('https://apis-starlights-team.koyeb.app/starlight/tiktoksearch?text=' + text)
+    let searchResults = response.data
     shuffleArray(searchResults);
-    let selectedResults = searchResults.splice(0, 7);
+    let selectedResults = searchResults.splice(0, 7)
 
     for (let result of selectedResults) {
       results.push({
@@ -41,7 +41,7 @@ let handler = async (message, { conn, text, usedPrefix, command }) => {
           videoMessage: await createVideoMessage(result.nowm)
         }),
         nativeFlowMessage: proto.Message.InteractiveMessage.NativeFlowMessage.fromObject({ buttons: [] })
-      });
+      })
     }
 
     const responseMessage = generateWAMessageFromContent(message.chat, {
@@ -59,18 +59,18 @@ let handler = async (message, { conn, text, usedPrefix, command }) => {
           })
         }
       }
-    }, { quoted: message });
-    await message.react(done);
+    }, { quoted: message })
+    await message.react(done)
 
-    await conn.relayMessage(message.chat, responseMessage.message, { messageId: responseMessage.key.id });
+    await conn.relayMessage(message.chat, responseMessage.message, { messageId: responseMessage.key.id })
   } catch (error) {
-    await conn.reply(message.chat, error.toString(), message);
+    await conn.reply(message.chat, error.toString(), message)
   }
-};
+}
 
-handler.help = ["tiktoksearch <txt>"];
-handler.estrellas = 1;
-handler.register = true;
-handler.tags = ["buscador"];
-handler.command = ["tiktoksearch", "tts", "tiktoks"];
-export default handler;
+handler.help = ["tiktoksearch <txt>"]
+handler.estrellas = 1
+handler.register = true
+handler.tags = ["buscador"]
+handler.command = ["tiktoksearch", "tts", "tiktoks"]
+export default handler
