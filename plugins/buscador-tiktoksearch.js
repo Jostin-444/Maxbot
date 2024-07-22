dce0858c6c809ff548a173ce8d725a9d8a582f74
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-07-21 23:48:14 -0500

    Actualizar buscador-tiktoksearch.js

--- plugins/buscador-tiktoksearch.js
+++ plugins/buscador-tiktoksearch.js
@@ -9,7 +9,7 @@ return videoMessage
 }
 async function shuffleArray(array) {
 for (let i = array.length - 1; i > 0; i--) {
-const j = Math.floor(Math.random() * (i + 1))
+let j = Math.floor(Math.random() * (i + 1))
 [array[i], array[j]] = [array[j], array[i]]
 }}
 try {
