54b80bee6ee61344d52322acd81a407914aeabeb
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-07-23 22:51:43 -0500

    Actualizar main-instalarluffy.js

--- plugins/main-instalarluffy.js
+++ plugins/main-instalarluffy.js
@@ -42,7 +42,7 @@ m.reply('npm start')
 }
 
 if (/^cafi$/i.test(m.text) ) {
-conn.reply(m.chat, '✏️ *Instalación por Cafirexos*\n(nosotros no tenemos tutorial pero eso les puede ayudar)\n\n• Pagina:\nhttps://cafirexos.com\n\n• Área de clientes:\nhttps://clientes.cafirexos.com\n\n• Panel:\nhttps://panel.cafirexos.com', m, rcanal)
+conn.reply(m.chat, '✏️ *Instalación por Cafirexos*\n(nosotros no tenemos tutorial pero eso les puede ayudar)\n\n• Área de clientes:\nhttps://clientes.cafirexos.com\n\n• Panel:\nhttps://panel.cafirexos.com', m, rcanal)
 await delay(2000 * 1)
 conn.sendMessage(m.chat, {image: {url: 'https://telegra.ph/file/fdeefe56c9454c09e9db6.jpg'}, caption: ''}, {quoted: fkontak})
 await delay(1000 * 1)
