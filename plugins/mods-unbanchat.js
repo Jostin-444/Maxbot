e7a400ee06b34b63dd2b4357076ac7c770ca9912
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-07-24 21:55:06 -0500

    Actualizar mods-unbanchat.js

--- plugins/mods-unbanchat.js
+++ plugins/mods-unbanchat.js
@@ -1,9 +1,9 @@
 let handler = async (m, { conn }) => {
-if (!(m.chat in global.db.data.chats)) return m.reply('╰⊱🌩⊱ 𝗘𝗥𝗥𝗢𝗥 ⊱🌩⊱╮\n\n🔵 *¡ESTE CHAT NO ESTÁ REGISTRADO!*')
+if (!(m.chat in global.db.data.chats)) return m.reply('🍭 *¡ESTE CHAT NO ESTÁ REGISTRADO!*')
 let chat = global.db.data.chats[m.chat]
-if (!chat.isBanned) return m.reply('╰⊱🌩⊱ 𝗘𝗥𝗥𝗢𝗥 ⊱🌩⊱╮\n\n🍬 *¡LA BOT NO ESTÁ BANEADA EN ESTE CHAT!*')
+if (!chat.isBanned) return m.reply('🍟 *¡LA BOT NO ESTÁ BANEADA EN ESTE CHAT!*')
 chat.isBanned = false
-await conn.reply(m.chat, '╰⊱🌺⊱ 𝗥𝗘𝗔𝗟𝗜𝗭𝗔𝗗𝗢 ⊱🌺⊱╮\n\n🍒 *¡LA BOT YA FUÉ DESBANEADA EN ESTE CHAT!*', m)
+await conn.reply(m.chat, '🚩 *¡LA BOT YA FUÉ DESBANEADA EN ESTE CHAT!*', m, fake)
 }
 handler.help = ['unbanchat'];
 handler.tags = ['mods'];
