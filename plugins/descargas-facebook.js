2367dafb6fdfd1b1d86dbc25a3f72ce020469cba
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-07-25 09:26:50 -0500

    Actualizar descargas-facebook.js

--- plugins/descargas-facebook.js
+++ plugins/descargas-facebook.js
@@ -1,4 +1,27 @@
-import fetch from 'node-fetch'
+import fg from 'api-dylux';
+let handler = async (m, { conn, text, args, usedPrefix, command }) => {
+
+if (!args[0]) throw `✳️ ${mssg.noLink('Facebook')}\n\n📌 ${mssg.example} :\n*${usedPrefix + command}* https://fb.watch/d7nB8-L-gR/`
+ await m.react(rwait);
+
+  try {
+    let result = await fg.fbdl(args[0]);
+    let tex = '🍟 *Aquí está su video de facebook¡!*';
+    conn.sendFile(m.chat, result.videoUrl, 'fb.mp4', tex, m, null, rcanal);
+   awair m.react(done);
+  } catch (error) {
+    await m.react('✖️');
+    conn.reply(m.chat, '❌️ Error inesperado', m, fake)
+  }
+};
+handler.help = ['facebook'].map(v => v + ' <url>');
+handler.tags = ['dl'];
+handler.command = /^((facebook|fb)(downloder|dl)?)$/i;
+handler.diamond = true;
+
+export default handler;
+
+/*import fetch from 'node-fetch'
 import { facebook } from '@xct007/frieren-scraper'
 
 let handler = async (m, { conn, args, usedPrefix, command }) => {
@@ -23,4 +46,4 @@ handler.tags = ['descargas']
 handler.command = ['fb', 'fbdl', 'facebookdl', 'facebook']
 handler.register = true 
 //handler.estrellas = 1
-export default handler
+export default handler*/
