a5d9da98ebdff14e1d5a0a0d47be16b48d7a9d23
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-07-25 10:22:58 -0500

    Actualizar descargas-facebook.js

--- plugins/descargas-facebook.js
+++ plugins/descargas-facebook.js
@@ -1,22 +1,27 @@
-const { savefrom, facebookdl, facebookdlv2 } = require('@bochilteam/scraper') 
+import fetch from 'node-fetch'
+// import { facebook } from '@xct007/frieren-scraper'
+import { savefrom, facebookdl, facebookdlv2 } = require('@bochilteam/scraper') 
 
-let handler = async (m, { conn, text, usedPrefix, command }) => {
-
-if (!text) return conn.reply(m.chat, `🚩 *Ingrese un enlace de facebook*\n\nEjemplo, !fb https://fb.watch/kAOXy3wf2L/?mibextid=Nif5oz`, m)
-if (!text.includes('facebook')) {
-return m.reply(facebook.text2)
-}
+let handler = async (m, { conn, args, usedPrefix, command }) => {
+if (!args[0]) return conn.reply(m.chat, `🚩 Ingresa el enlace del vídeo de Facebook junto al comando.\n\nEjemplo:\n> *${usedPrefix + command}* https://www.facebook.com/share/v/Mqwsfxs8agAHsx4m/?mibextid=xfxF2i`, m, rcanal)
 try {
-await m.reply('Cargando') 
-const { result } = await facebookdl(text).catch(async () => await facebookdlv2(text)).catch(async () => await savefrom(text))
-for (const { url, isVideo } of result.reverse()) await conn.sendMessage(m.chat, {video: {url: url}, caption: '🍟 Video de facebook'}, {quoted: m})
-} catch (e) {
-m.reply(`🚩 *Ocurrió un fallo*\n\n> ${e}`)
-}
-
-}
-handler.help = ['fb', 'fbdl', 'facebook']
-handler.command = ['fb', 'fbdl', 'facebook']
-handler.tags = ['downloader']
-
-module.exports = handler
+await m.react(rwait)
+conn.reply(m.chat, '🚩 Enviando el video...', m, {
+contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, showAdAttribution: true,
+title: packname,
+body: wm,
+previewType: 0, thumbnail: icons,
+sourceUrl: channel }}})
+let { title, SD, HD } = await Scraper.fbdl(args[0])
+await conn.sendMessage(m.chat, { video: { url: SD || HD }, caption: '🍟 *Aquí está tu video*' }, { quoted: fkontak})
+await m.react(done)
+} catch {
+await m.react(error)
+conn.reply(m.chat, '❌️ Ocurrió un error', m, fake)
+}}
+handler.help = ['facebook <url fb>']
+handler.tags = ['descargas']
+handler.command = ['fb', 'fbdl', 'facebookdl', 'facebook']
+handler.register = true 
+//handler.estrellas = 1
+export default handler
