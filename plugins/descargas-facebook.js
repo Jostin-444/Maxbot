dbcb34bd1504c222706a4e844e31e11b380148ce
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-07-25 09:03:59 -0500

    Actualizar descargas-facebook.js

--- plugins/descargas-facebook.js
+++ plugins/descargas-facebook.js
@@ -7,10 +7,19 @@ if (!text.includes('facebook')) {
 return conn.reply(m.chat, '🚩 Enlace invalido', m, rcanal)
 }
 try {
+await m.react(rwait)
+conn.reply(m.chat, '🍟 Enviando el video....', m, {
+contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, showAdAttribution: true,
+title: packname,
+body: wm,
+previewType: 0, thumbnail: icons,
+sourceUrl: channel }}})
 // await conn.reply('Cargando') 
 const { result } = await facebookdl(text).catch(async () => await facebookdlv2(text)).catch(async () => await savefrom(text))
 for (const { url, isVideo } of result.reverse()) await conn.sendMessage(m.chat, {video: {url: url}, caption: '🍟 Video de facebook'}, {quoted: fkontak})
+await m.react(done)
 } catch (e) {
+await m.react(error)
 m.reply(`🚩 *Ocurrió un fallo*\n\n> ${e}`)
 }
 
