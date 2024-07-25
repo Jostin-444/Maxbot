59f3ba0e97719f2fcca201427be7efef5dfd9b8f
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-07-25 09:05:07 -0500

    Actualizar descargas-facebook.js

--- plugins/descargas-facebook.js
+++ plugins/descargas-facebook.js
@@ -2,24 +2,15 @@ const { savefrom, facebookdl, facebookdlv2 } = require('@bochilteam/scraper')
 
 let handler = async (m, { conn, text, usedPrefix, command }) => {
 
-if (!text) return conn.reply(m.chat, `🚩 *Ingrese un enlace de facebook*\n\nEjemplo, !fb https://fb.watch/kAOXy3wf2L/?mibextid=Nif5oz`, m, rcanal)
+if (!text) return conn.reply(m.chat, `🚩 *Ingrese un enlace de facebook*\n\nEjemplo, !fb https://fb.watch/kAOXy3wf2L/?mibextid=Nif5oz`, m)
 if (!text.includes('facebook')) {
-return conn.reply(m.chat, '🚩 Enlace invalido', m, rcanal)
+return conn.reply(m.chat, '🚩 Enlace ', m, rcanal)
 }
 try {
-await m.react(rwait)
-conn.reply(m.chat, '🍟 Enviando el video....', m, {
-contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, showAdAttribution: true,
-title: packname,
-body: wm,
-previewType: 0, thumbnail: icons,
-sourceUrl: channel }}})
 // await conn.reply('Cargando') 
 const { result } = await facebookdl(text).catch(async () => await facebookdlv2(text)).catch(async () => await savefrom(text))
 for (const { url, isVideo } of result.reverse()) await conn.sendMessage(m.chat, {video: {url: url}, caption: '🍟 Video de facebook'}, {quoted: fkontak})
-await m.react(done)
 } catch (e) {
-await m.react(error)
 m.reply(`🚩 *Ocurrió un fallo*\n\n> ${e}`)
 }
 
