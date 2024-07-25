59233a5daae031d18ac966ffc3bd7d1f46cb9ffd
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-07-25 09:05:19 -0500

    Actualizar descargas-facebook.js

--- plugins/descargas-facebook.js
+++ plugins/descargas-facebook.js
@@ -4,22 +4,19 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
 
 if (!text) return conn.reply(m.chat, `🚩 *Ingrese un enlace de facebook*\n\nEjemplo, !fb https://fb.watch/kAOXy3wf2L/?mibextid=Nif5oz`, m)
 if (!text.includes('facebook')) {
-return conn.reply(m.chat, '🚩 Enlace ', m, rcanal)
+return m.reply(facebook.text2)
 }
 try {
-// await conn.reply('Cargando') 
+await m.reply('Cargando') 
 const { result } = await facebookdl(text).catch(async () => await facebookdlv2(text)).catch(async () => await savefrom(text))
-for (const { url, isVideo } of result.reverse()) await conn.sendMessage(m.chat, {video: {url: url}, caption: '🍟 Video de facebook'}, {quoted: fkontak})
+for (const { url, isVideo } of result.reverse()) await conn.sendMessage(m.chat, {video: {url: url}, caption: '🍟 Video de facebook'}, {quoted: m})
 } catch (e) {
 m.reply(`🚩 *Ocurrió un fallo*\n\n> ${e}`)
 }
 
 }
-handler.help = ['fb']
-handler.tags = ['descargas']
-handler.command = /^(facebook|fb|facebookdl|fbdl)$/i
+handler.help = ['fb', 'fbdl', 'facebook']
+handler.command = ['fb', 'fbdl', 'facebook']
+handler.tags = ['downloader']
 
-handler.register = true
-handler.estrellas = 1
-
-export default handler
+module.exports = handler
