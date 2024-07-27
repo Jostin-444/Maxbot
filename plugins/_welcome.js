ea809b5bd7b9af609927d4a3a347441df4e7318c
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-07-27 15:22:00 -0500

    Actualizar _welcome.js

--- plugins/_welcome.js
+++ plugins/_welcome.js
@@ -1,4 +1,4 @@
-,import {WAMessageStubType} from '@whiskeysockets/baileys'
+import {WAMessageStubType} from '@whiskeysockets/baileys'
 import fetch from 'node-fetch'
 
 export async function before(m, {conn, participants, groupMetadata}) {
