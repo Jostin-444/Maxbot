bc311be0bfc458611906bbb83e11adbbb221b6a6
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-07-27 22:27:56 -0500

    Actualizar _welcome.js

--- plugins/_welcome.js
+++ plugins/_welcome.js
@@ -7,30 +7,6 @@ export async function before(m, {conn, participants, groupMetadata}) {
   let chat = global.db.data.chats[m.chat]
 
   if (chat.welcome && m.messageStubType == 27) {
-    let welcome = `Bienvenido @${m.messageStubParameters[0].split`@`[0]}`
-await conn.sendMini(m.chat, packname, textbot, welcome, img, img, redes, fkontak)
-  }
-
-  if (chat.welcome && m.messageStubType == 28) {
-    let bye = `Adios @${m.messageStubParameters[0].split`@`[0]}`
-await conn.sendMini(m.chat, packname, textbot, bye, img, img, redes, fkontak)
-  }
-
-  if (chat.welcome && m.messageStubType == 32) {
-    let kick = `Adios @${m.messageStubParameters[0].split`@`[0]}`
-await conn.sendMini(m.chat, packname, textbot, kick, img, img, redes, fkontak)
-}}
-
-
-/*import {WAMessageStubType} from '@whiskeysockets/baileys'
-import fetch from 'node-fetch'
-
-export async function before(m, {conn, participants, groupMetadata}) {
-  if (!m.messageStubType || !m.isGroup) return !0;
-  let img = welcome
-  let chat = global.db.data.chats[m.chat]
-
-  if (chat.welcome && m.messageStubType == 27) {
     let welcome = `┌─★ *Ai Yaemori - MD* \n│「 Bienvenido 」\n└┬★ 「 @${m.messageStubParameters[0].split`@`[0]} 」\n   │✑  Bienvenido a\n   │✑  ${groupMetadata.subject}\n   └───────────────┈ ⳹`
 await conn.sendMini(m.chat, packname, textbot, welcome, img, img, redes, fkontak)
   }
@@ -43,4 +19,4 @@ await conn.sendMini(m.chat, packname, textbot, bye, img, img, redes, fkontak)
   if (chat.welcome && m.messageStubType == 32) {
     let kick = `┌─★ *Ai Yaemori - MD* \n│「 ADIOS 👋 」\n└┬★ 「 @${m.messageStubParameters[0].split`@`[0]} 」\n   │✑  Se fue\n   │✑ Jamás te quisimos aquí\n   └───────────────┈ ⳹`
 await conn.sendMini(m.chat, packname, textbot, kick, img, img, redes, fkontak)
-}}*/
+}}
