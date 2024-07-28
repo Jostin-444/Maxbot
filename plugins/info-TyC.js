f13bf5bb16f0e09155c2090bdd0ceef52b6f9a2b
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-07-28 15:35:55 -0500

    Actualizar info-TyC.js

--- plugins/info-TyC.js
+++ plugins/info-TyC.js
@@ -20,6 +20,6 @@ ${global.correo}
 ✰ ${global.packname}`, m, rcanal)
 
 }
-handler.customPrefix = /términos y condiciones y privacidad|terminosycondicionesyprivacidad|terminosycondiciones|terminos y condiciones y privacidad|terminos y condiciones|terminos y condiciones|terminos de uso|Terminos de uso|Terminó se uso|términos de uso|Términos de uso|Términos y condiciones/i;
-handler.command = new RegExp;
-export default handler;
+handler.customPrefix = /términos y condiciones y privacidad|terminosycondicionesyprivacidad|terminosycondiciones|terminos y condiciones y privacidad|terminos y condiciones|terminos y condiciones|terminos de uso|Terminos de uso|Terminó se uso|términos de uso|Términos de uso|Términos y condiciones/i
+handler.command = new RegExp
+export default handler
