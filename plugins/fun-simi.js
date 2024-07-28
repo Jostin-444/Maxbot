5ed4d289596e29537c8c2163dbb3c1e9fffab6aa
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-07-27 21:02:01 -0500

    Actualizar fun-simi.js

--- plugins/fun-simi.js
+++ plugins/fun-simi.js
@@ -8,7 +8,7 @@ if (!text) conn.reply(m.chat, '🍟 Te faltó el texto para hablar con *Ai Yaemo
 try {
 // await m.react(emojis)
 const resSimi = await simitalk(text);
-conn.sendMessage(m.chat, { text: resSimi.resultado.simsimi }, { quoted: estilo });
+conn.sendMessage(m.chat, { text: resSimi.resultado.simsimi }, { quoted: m });
 await m.react(emojis)
 } catch {
 throw '🚩 *Ocurrió un error*';
