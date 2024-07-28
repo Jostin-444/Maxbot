7a2ad57f3933a2565e375e3fe67b8e1bfeb5bc63
DarkesJs <theyaemoribot@gmail.com>
2024-07-28 16:46:45 -0500

    Actualizar gc-delete.js

--- plugins/gc-delete.js
+++ plugins/gc-delete.js
@@ -1,20 +1,14 @@
 let handler = async (m, { conn, usedPrefix, command, isBotAdmin, isAdmin }) => {
 
 if (!m.quoted) return conn.reply(m.chat, `🚩 Responde al mensaje que deseas eliminar.`, m, rcanal)
-if (!isBotAdmin) return conn.reply(m.chat, `🌼 *No soy admin, no puedo eliminar mensajes de otro miembro*`, m, rcanal, )
+// if (!isBotAdmin) return conn.reply(m.chat, `🌼 *No soy admin, no puedo eliminar mensajes de otro miembro*`, m, rcanal, )
 try {
 let delet = m.message.extendedTextMessage.contextInfo.participant
 let bang = m.message.extendedTextMessage.contextInfo.stanzaId
-/*return conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: bang, participant: delet }})
+return conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: bang, participant: delet }})
 } catch {
 return conn.sendMessage(m.chat, { delete: m.quoted.vM.key })
-}}*/
-
-if (isAdmin) {
-await conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: bang, participant: delet }})
-await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
-} else if (!bot.restrict) return conn.reply(m.chat, `*¡Esta característica esta desactivada!*`, m, rcanal, )
-}
+}}
 
 handler.help = ['delete']
 handler.tags = ['grupo']
