61dfc68c9dcc88cd9b73a61a1ca17df5a86d198b
DarkesJs <theyaemoribot@gmail.com>
2024-07-28 17:09:37 -0500

    Actualizar rpg-retirar.js

--- plugins/rpg-retirar.js
+++ plugins/rpg-retirar.js
@@ -20,6 +20,6 @@ await m.reply(`🚩 Retiraste *${count} ⭐ Estrellas* del Banco.`)}
 
 handler.help = ['retirar']
 handler.tags = ['rpg']
-handler.command = ['withdraw', 'retirar', 'wd']
+handler.command = ['withdraw', 'retirar', 'wd', 'sacar']
 handler.register = true 
 export default handler
