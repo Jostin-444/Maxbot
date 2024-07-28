4d26a867eca3f0d3d142f5b6f73d7e77bfe55a4d
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-07-28 10:16:54 -0500

    Actualizar buscador-githubsearch.js

--- plugins/buscador-githubsearch.js
+++ plugins/buscador-githubsearch.js
@@ -1,7 +1,7 @@
 import fetch from 'node-fetch'
 var handler = async (m, { conn, text, usedPrefix, command }) => {
 
-if (!text) return conn.reply(m.chat, `🚩 *Ingrese el nombre de un repositorio de github*\n\nEjemplo, !${command} Ai-Yaemori`, m, rcanal)
+if (!text) return conn.reply(m.chat, `🚩 *Ingrese el nombre de un repositorio de github*\n\nEjemplo, ${usedPrefix + command} Ai-Yaemori`, m, rcanal)
 
 try {
 
