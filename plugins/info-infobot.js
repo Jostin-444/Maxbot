0b0f5b56322f07a001e05aed94f6dd8f1fd668fb
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-07-27 20:10:16 -0500

    Actualizar info-infobot.js

--- plugins/info-infobot.js
+++ plugins/info-infobot.js
@@ -79,7 +79,7 @@ yaemori += `│ 💭 *${chats.length}* Chats Totales\n`
 yaemori += `╰─⬣\n\n`
 yaemori += `╭─⬣「 *NodeJS Uso de memoria* 」⬣\n`
 yaemori += `${'```' + Object.keys(used).map((key, _, arr) => `│ ${key.padEnd(Math.max(...arr.map(v => v.length)), ' ')}: ${format(used[key])}`).join('\n') + '```'}\n`
-luffy += `╰─⬣`
+yaemori += `╰─⬣`
 
 await conn.reply(m.chat, yaemori, fkontak, { contextInfo: { mentionedJid: [owner[0][0] + '@s.whatsapp.net'], externalAdReply: { mediaUrl: false, mediaType: 1, description: false, title: '↷✦╎Info - Bot╎🚩˖ ⸙',body: packname, previewType: 0, thumbnail: icons, sourceUrl: redes}}})
 // await conn.sendFile(m.chat, imagen1, 'luffy.jpg', luffy, fkontak, null, rcanal)
