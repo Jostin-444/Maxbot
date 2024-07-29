f9fb15f7016cb57b24e934fc3b99e2385955263c
DarkesJs <theyaemoribot@gmail.com>
2024-07-29 18:42:21 -0500

    Actualizar _welcome.js

--- plugins/_welcome.js
+++ plugins/_welcome.js
@@ -4,7 +4,7 @@ import fetch from 'node-fetch'
 export async function before(m, {conn, participants, groupMetadata}) {
   if (!m.messageStubType || !m.isGroup) return !0
   let apii = await this.getFile(pp)
-  pp = await this.profilePictureUrl(nombre, 'image')
+  let pp = await this.profilePictureUrl(nombre, 'image')
   let vn = 'https://qu.ax/cUYg.mp3'
   let img = imagen1
   let chat = global.db.data.chats[m.chat]
