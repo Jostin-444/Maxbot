7df024b3d2ae65d3b16606fe99e2cca752628321
DarkesJs <theyaemoribot@gmail.com>
2024-07-28 20:42:43 -0500

    Actualizar descargas-facebook.js

--- plugins/descargas-facebook.js
+++ plugins/descargas-facebook.js
@@ -3,10 +3,12 @@ import { igdl } from 'ruhend-scraper';
 const handler = async (m, { text, conn, args, usedPrefix, command }) => {
 if (!args[0]) {
 return conn.reply(m.chat, '*`Ingresa Un Link De Facebook`*', m)}
-let res;
+let res
 try {
-res = await igdl(args[0]);
-} catch (error) {
+await m.react(rwait)
+res = await igdl(args[0])
+} catch {
+await m.react(error)
 return conn.reply(m.chat, '🚩 *Error al obtener datos. Verifica el enlace.*', m)}
 let result = res.data;
 if (!result || result.length === 0) {
@@ -15,7 +17,8 @@ let data
 try {
 await m.react(rwait)
 data = result.find(i => i.resolution === "720p (HD)") || result.find(i => i.resolution === "360p (SD)")
-} catch (error) {
+} catch {
+await m.react(error)
 return conn.reply(m.chat, '🚩 *Error al procesar los datos.*', m, rcanal)}
 if (!data) {
 return conn.reply(m.chat, '🚩 *No se encontró una resolución adecuada.*', m, rcanal)}
@@ -24,7 +27,8 @@ try {
 await m.react(rwait)
 await conn.sendMessage(m.chat, { video: { url: video }, caption: null, fileName: 'fb.mp4', mimetype: 'video/mp4' }, { quoted: m })
 await m.react(done)
-} catch (error) {
+} catch {
+await m.react(error)
 return conn.reply(m.chat, '🚩 *Error al enviar el video.*', m, rcanal)}}
 
 handler.help = ['facebook', 'fb']
