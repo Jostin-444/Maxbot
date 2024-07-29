459e02992344793c6e9c4617593590790383898a
DarkesJs <theyaemoribot@gmail.com>
2024-07-28 20:00:53 -0500

    Actualizar descargas-ig.js

--- plugins/descargas-ig.js
+++ plugins/descargas-ig.js
@@ -1,3 +1,37 @@
+// *`Instagram Downloader 🐢`*
+
+import { igdl } from "ruhend-scraper";
+
+let handler = async (m, { args, conn }) => { 
+    if (!args[0]) {
+        return conn.reply(m.chat, '*`Ingresa un link de Instagram`*', m);
+    }
+    
+    try {
+        await m.react('🕑');
+        
+        let res = await igdl(args[0]);
+        let data = res.data; 
+        
+        for (let media of data) {
+            await new Promise(resolve => setTimeout(resolve, 2000));
+            
+            await conn.sendFile(m.chat, media.url, 'instagram.mp4', null, m); 
+        }
+    } catch {
+        await m.react('❌');
+    }
+}
+
+handler.command = ['instagram'];
+handler.tags = ['descargas'];
+handler.help = ['instagram'];
+
+export default handler;
+
+// *_`Package`_* 
+// "ruhend-scraper": "^*",
+
 /* import Starlights from '@StarlightsTeam/Scraper'
 let listo = '🚩 Tu video de instagram'
 let handler = async (m, { conn, args, usedPrefix, command }) => {
