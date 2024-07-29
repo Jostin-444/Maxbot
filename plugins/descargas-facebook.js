f457ceadb1aa06ddb6465bb50ef229062cb3d08c
DarkesJs <theyaemoribot@gmail.com>
2024-07-28 20:59:22 -0500

    Actualizar descargas-facebook.js

--- plugins/descargas-facebook.js
+++ plugins/descargas-facebook.js
@@ -13,11 +13,24 @@ previewType: 0, thumbnail: icons,
 sourceUrl: channel }}})
 await m.react(rwait)
 res = await igdl(args[0])
+} catch {
+await m.react(error)
+return conn.reply(m.chat, '🚩 *Error al obtener datos. Verifica el enlace.*', m)}
+let result = res.data
+if (!result || result.length === 0) {
+return conn.reply(m.chat, '🚩 *No se encontraron resultados.*', m)}
 let data
 try {
 await m.react(rwait)
 data = result.find(i => i.resolution === "720p (HD)") || result.find(i => i.resolution === "360p (SD)")
+} catch {
+await m.react(error)
+return conn.reply(m.chat, '🚩 *Error al procesar los datos.*', m, rcanal)}
+if (!data) {
+return conn.reply(m.chat, '🚩 *No se encontró una resolución adecuada.*', m, rcanal)}
 let video = data.url
+try {
+await m.react(rwait)
 await conn.sendMessage(m.chat, { video: { url: video }, caption: '🍟 *Tu video de facebook.*' + textbot, fileName: 'fb.mp4', mimetype: 'video/mp4' }, { quoted: fkontak })
 await m.react(done)
 } catch {
