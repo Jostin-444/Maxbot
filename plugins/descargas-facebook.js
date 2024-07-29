87d1132066b5b0192971bbf21f3ac3caa8e388a2
DarkesJs <theyaemoribot@gmail.com>
2024-07-28 20:46:58 -0500

    Actualizar descargas-facebook.js

--- plugins/descargas-facebook.js
+++ plugins/descargas-facebook.js
@@ -5,6 +5,12 @@ if (!args[0]) {
 return conn.reply(m.chat, '*`Ingresa Un Link De Facebook`*', m, rcanal)}
 let res
 try {
+conn.reply(m.chat, `🕒 *Descargando su video de facebook.*`, m, {
+contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, showAdAttribution: true,
+title: packname,
+body: wm,
+previewType: 0, thumbnail: icons,
+sourceUrl: channel }}})
 await m.react(rwait)
 res = await igdl(args[0])
 } catch {
@@ -15,6 +21,12 @@ if (!result || result.length === 0) {
 return conn.reply(m.chat, '🚩 *No se encontraron resultados.*', m)}
 let data
 try {
+conn.reply(m.chat, `🕒 *Descargando su video de facebook.*`, m, {
+contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, showAdAttribution: true,
+title: packname,
+body: wm,
+previewType: 0, thumbnail: icons,
+sourceUrl: channel }}})
 await m.react(rwait)
 data = result.find(i => i.resolution === "720p (HD)") || result.find(i => i.resolution === "360p (SD)")
 } catch {
@@ -24,6 +36,12 @@ if (!data) {
 return conn.reply(m.chat, '🚩 *No se encontró una resolución adecuada.*', m, rcanal)}
 let video = data.url
 try {
+conn.reply(m.chat, `🕒 *Descargando su video de facebook.*`, m, {
+contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, showAdAttribution: true,
+title: packname,
+body: wm,
+previewType: 0, thumbnail: icons,
+sourceUrl: channel }}})
 await m.react(rwait)
 await conn.sendMessage(m.chat, { video: { url: video }, caption: '🍟 *Tu video de facebook.*', fileName: 'fb.mp4', mimetype: 'video/mp4' }, { quoted: fkontak })
 await m.react(done)
