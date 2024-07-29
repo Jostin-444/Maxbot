4e001a83d0993f7b7ff29c7b619da76fcdcd78ca
DarkesJs <theyaemoribot@gmail.com>
2024-07-28 20:15:15 -0500

    Actualizar descargas-ig.js

--- plugins/descargas-ig.js
+++ plugins/descargas-ig.js
@@ -15,7 +15,7 @@ let res = await igdl(args[0])
 let data = res.data       
 for (let media of data) {
 await new Promise(resolve => setTimeout(resolve, 2000))           
-await conn.sendFile(m.chat, media.url, 'instagram.mp4', '🍟 *Tu video de instagram.*', rcanal)
+await conn.sendFile(m.chat, media.url, 'instagram.mp4', '🍟 *Tu video de instagram.*', fkontak)
 }} catch {
 await m.react(error)
 conn.reply(m.chat, '🚩 Ocurrió un error.', m, fake)}}
