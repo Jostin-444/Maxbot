372e83eb17fc94c24efdd45fffa2bb1f626b8c23
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-07-28 20:31:39 -0500

    Actualizar descargas-facebook.js

--- plugins/descargas-facebook.js
+++ plugins/descargas-facebook.js
@@ -1,4 +1,52 @@
-import fetch from 'node-fetch'
+
+
+ import { igdl } from 'ruhend-scraper';
+
+const handler = async (m, { text, conn, args, usedPrefix, command }) => {
+  if (!args[0]) {
+    return conn.reply(m.chat, '*`Ingresa Un Link De Facebook`*', m);
+  }
+
+  let res;
+  try {
+    res = await igdl(args[0]);
+  } catch (error) {
+    return conn.reply(m.chat, '*`Error al obtener datos. Verifica el enlace.`*', m);
+  }
+
+  let result = res.data;
+  if (!result || result.length === 0) {
+    return conn.reply(m.chat, '*`No se encontraron resultados.`*', m);
+  }
+
+  let data;
+  try {
+    data = result.find(i => i.resolution === "720p (HD)") || result.find(i => i.resolution === "360p (SD)");
+  } catch (error) {
+    return conn.reply(m.chat, '*`Error al procesar los datos.`*', m);
+  }
+
+  if (!data) {
+    return conn.reply(m.chat, '*`No se encontró una resolución adecuada.`*', m);
+  }
+
+  let video = data.url;
+  
+  try {
+    await conn.sendMessage(m.chat, { video: { url: video }, caption: null, fileName: 'fb.mp4', mimetype: 'video/mp4' }, { quoted: m });
+  } catch (error) {
+    return conn.reply(m.chat, '*`Error al enviar el video.`*', m);
+  }
+};
+
+handler.command = /^(facebook|fb)$/i;
+
+export default handler;       
+
+                                                                                                                                                                                                                                                      *_`package  :`_*
+   "ruhend-scraper": "^*",
+
+/*import fetch from 'node-fetch'
 import { facebookdl, facebookdlv2 } from '@bochilteam/scraper'
 
 let handler = async (m, { conn, args, usedPrefix, command }) => {
@@ -23,4 +71,4 @@ handler.tags = ['descargas']
 handler.command = ['fb', 'fbdl', 'facebookdl', 'facebook']
 handler.register = true 
 //handler.estrellas = 1
-export default handler
+export default handler*/
