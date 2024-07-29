3245a4eb920d4dc6b23490611c68108e460b92e4
DarkesJs <theyaemoribot@gmail.com>
2024-07-28 20:08:55 -0500

    Actualizar descargas-ig.js

--- plugins/descargas-ig.js
+++ plugins/descargas-ig.js
@@ -6,12 +6,18 @@ let handler = async (m, { args, conn }) => {
 if (!args[0]) {
 return conn.reply(m.chat, '*`Ingresa un link de Instagram`*', m, rcanal)}
 try {
-await m.react(rwait)      
+await m.react(rwait)
+conn.reply(m.chat, `🕒 *Enviando El Video...*`, m, {
+contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, showAdAttribution: true,
+title: packname,
+body: wm,
+previewType: 0, thumbnail: icons,
+sourceUrl: channel }}})      
 let res = await igdl(args[0])
 let data = res.data       
 for (let media of data) {
 await new Promise(resolve => setTimeout(resolve, 2000))           
-await conn.reply(m.chat, media.url, 'instagram.mp4', m, rcanal)
+await conn.sendFile(m.chat, media.url, 'instagram.mp4', '🍟 *Aquí está tu video.*', fkontak)
 }} catch {
 await m.react(error)
 conn.reply(m.chat, '🚩 Ocurrió un error.', m, fake)}}
