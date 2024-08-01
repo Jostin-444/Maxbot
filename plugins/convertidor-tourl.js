1a66960d47eaf86b28a42388dd04fd00d375a747
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-07-31 20:33:11 -0400

    Actualizar convertidor-tourl.js

--- plugins/convertidor-tourl.js
+++ plugins/convertidor-tourl.js
@@ -5,33 +5,27 @@ import fetch from 'node-fetch'
 let handler = async (m) => {
   let q = m.quoted ? m.quoted : m
   let mime = (q.msg || q).mimetype || ''
-  if (!mime) return conn.reply(m.chat, '💥 Responde a una *Imagen* o *Vídeo.*', m, rcanal)
-  await m.react(rwait)
+  if (!mime) return conn.reply(m.chat, '🍭 Responde a una *Imagen* o *Vídeo.*', m, rcanal)
+  await m.react('🕓')
   try {
-  conn.reply(m.chat, global.wait, m, {
-  contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, showAdAttribution: true,
-  title: packname,
-  body: wm,
-  previewType: 0, thumbnail: icons,
-  sourceUrl: channel }}})
   let media = await q.download()
   let isTele = /image\/(png|jpe?g|gif)|video\/mp4/.test(mime)
   let link = await (isTele ? uploadImage : uploadFile)(media)
   let img = await (await fetch(`${link}`)).buffer()
-  let txt = `乂  *L I N K - E N L A C E*  乂\n\n`
-      txt += `*» Enlace* : ${link}\n`
-      txt += `*» Acortado* : ${await shortUrl(link)}\n`
-      txt += `*» Tamaño* : ${formatBytes(media.length)}\n`
-      txt += `*» Expiración* : ${isTele ? 'No expira' : 'Desconocido'}\n\n`
-      txt += `> *${dev}*`
+  let txt = '`T E L E G R A P H`\n\n'
+      txt += `*🔖 Enlace* : ${link}\n`
+      txt += `*🔖 Acortado* : ${await shortUrl(link)}\n`
+      txt += `*🔖 Tamaño* : ${formatBytes(media.length)}\n`
+      txt += `*🔖 Expiración* : ${isTele ? 'No expira' : 'Desconocido'}\n\n`
+      txt += `🐈‍⬛ *${textbot}*`
 
-await conn.sendFile(m.chat, img, 'thumbnail.jpg', txt, m, fkontak, rcanal)
-await m.react(done)
+await conn.sendFile(m.chat, img, 'thumbnail.jpg', txt, m, null, rcanal)
+await m.react('✅')
 } catch {
-await m.react(error)
+await m.react('✖️')
 }}
 handler.help = ['tourl']
-handler.tags = ['transformador']
+handler.tags = ['tools']
 handler.command = /^(tourl|upload)$/i
 export default handler
 
