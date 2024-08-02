9cdfafc4a1cdc355b45ab7e86e1ef12d65bf40ae
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-08-02 13:38:14 -0400

    Actualizar main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -59,7 +59,7 @@ const defaultMenu = {
 \t*L I S T A  -  D E  -  C O M A N D O S* 
 `.trimStart(),
       header: '╭───═[ %category ]═────⋆\n│╭───────────────···',
-  body: '││ %cmd\n',
+  body: '││ %cmd %isdiamond %isPremium\n',
   footer: '│╰────────────────···\n╰────────═┅═─────────\n',
   after: `> ${dev}`,
 }
