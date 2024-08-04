ec2abe5953ebad6bcaa3c9a5f216ee0153b0dac2
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-08-04 11:16:11 -0400

    Actualizar Prueba.js

---
+++ plugins/Prueba.js
@@ -0,0 +1,55 @@
+import JavaScriptObfuscator from "javascript-obfuscator"
+
+let handler = async (m, {
+    args
+}) => {
+    try {
+        const modes = ["low", "high"]
+        const usage = "Responde un código!!!\n\n*Example:*\n.obfuscate high"
+        if (!m.quoted) return m.reply(usage)
+        const type = args.shift().toLowerCase()
+        if (!modes.includes(type)) return m.reply(usage)
+        const message = type === "high" ? await Encrypt(m.quoted.text) : await Decrypt(m.quoted.text)
+        if (args.length >= 2) {
+            const texts = args.slice(1).join(" ")
+            const response = type === "high" ? await Encrypt(texts) : await Decrypt(texts)
+            return m.reply(response)
+        }
+
+        return m.reply(message)
+
+    } catch (e) {
+        await m.reply(eror)
+    }
+}
+handler.help = ['encrypt']
+handler.tags = ['tools']
+handler.command = /^(encrypt|enc)$/i
+handler.limit = true
+export default handler
+
+async function Encrypt(query) {
+    const obfuscationResult = JavaScriptObfuscator.obfuscate(query, {
+        compact: true,
+        controlFlowFlattening: true,
+        controlFlowFlatteningThreshold: 1,
+        numbersToExpressions: true,
+        simplify: true,
+        stringArrayShuffle: true,
+        splitStrings: true,
+        stringArrayThreshold: 1,
+        sourceMap: false,
+        sourceMapMode: "separate",
+    })
+
+    return obfuscationResult.getObfuscatedCode()
+}
+
+async function Decrypt(encryptedCode) {
+    const decryptedCode = JavaScriptObfuscator.obfuscate(encryptedCode, {
+        compact: false,
+        controlFlowFlattening: true,
+    }).getObfuscatedCode()
+
+    return decryptedCode
+}
