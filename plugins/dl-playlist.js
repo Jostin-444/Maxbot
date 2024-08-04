99ff4f9c2adafdc45f85946b2b47e4be2ba385a3
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-08-04 18:51:38 -0400

    Actualizar dl-playlist.js

--- plugins/dl-playlist.js
+++ plugins/dl-playlist.js
@@ -30,7 +30,7 @@ let handler = async (m, { conn, usedPrefix, text, args, command }) => {
         });
     }
 
-    await conn.sendListB(m.chat, '*GENESIS BOT*🔎', `\n 📀 Resultados de:\n *${text}*`, `OPCIONES`, ytres[0].image, listSections, m);
+    await conn.sendListB(m.chat, '*GENESIS BOT*🔎', `\n 📀 Resultados de:\n *${text}*`, `OPCIONES`, null, [['CANAL 🐈‍⬛', `${canal}`]], ytres[0].image, listSections, m);
 };
 
 handler.help = ['play2']
