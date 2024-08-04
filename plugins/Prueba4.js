a88d50645cdbad584df1790ab24f8a5082e99b45
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-08-04 11:30:28 -0400

    Actualizar Prueba4.js

--- plugins/Prueba4.js
+++ plugins/Prueba4.js
@@ -1,21 +1,68 @@
-//import { bold } from "chalk"
-let handler = async (m, { conn, text }) => {
+import fetch from "node-fetch";
 
-   if (!text) return m.reply('_Masukkan Nama Grup!_')
-   try{
-    m.reply(wait)
-    let group = await conn.groupCreate(text, [m.sender])
-    let link = await conn.groupInviteCode(group.gid)
-    let url = 'https://chat.whatsapp.com/' + link;
- /// console.log(chalk.bold.red('Membuat Grup: ' + group.gid + '\nNama Grup: ' + text + '\n\nViolet'))
-    m.reply('_Berhasil Membuat Grup *' + text + '*_\n\n*Nama:* ' + text + '\n*ID:* ' + group.gid + '\n*Link:* ' + url)
-       } catch (e) {
-    m.reply(`Error`)
-  }
+let handler = async (m, {
+    conn,
+    args,
+    usedPrefix,
+    command
+}) => {
+    if (!m.quoted) return m.reply("Responde texto/audio para usar este gpt");
+
+    try {
+        if (m.quoted.text) {
+            let res = await gptChat(m.quoted.text);
+            await m.reply(res.data);
+        } else if (m.quoted.mimetype.includes("audio")) {
+            let audioBuff = await m.quoted.download();
+            let res = await gptAudio(audioBuff);
+            await m.reply(res.data);
+        }
+    } catch (e) {
+        console.error('An error occurred:', e.message);
+        await m.reply('Error occurred. Please try again.');
+    }
+};
+
+handler.help = ["chatgbt"];
+handler.tags = ["gpt"];
+handler.command = /^(chatgbt)$/i;
+
+export default handler;
+
+/* New Line */
+async function gptAudio(audioBuffer) {
+    try {
+        const data = new FormData();
+        const blob = new Blob([audioBuffer.toArrayBuffer()], { type: 'audio/mpeg' });
+        data.append('_wpnonce', '220ba5b841');
+        data.append('post_id', '7');
+        data.append('action', 'wpaicg_chatbox_message');
+        data.append('audio', blob, 'wpaicg-chat-recording.wav');
+        const response = await fetch('https://chatgbt.one/wp-admin/admin-ajax.php', { method: 'POST', body: data });
+
+        if (!response.ok) throw new Error('Network response was not ok');
+
+        return await response.json();
+    } catch (error) {
+        console.error('An error occurred:', error.message);
+        throw error;
+    }
 }
-handler.help = ['buatgrup']
-handler.tags = ['owner']
-handler.command = /^((create|buat)(gc|grup|group))$/
-handler.owner = true
-handler.premium = false
-export default handler
+
+async function gptChat(message) {
+    try {
+        const data = new FormData();
+        data.append('_wpnonce', '220ba5b841');
+        data.append('post_id', '7');
+        data.append('action', 'wpaicg_chatbox_message');
+        data.append('message', message);
+        const response = await fetch('https://chatgbt.one/wp-admin/admin-ajax.php', { method: 'POST', body: data });
+
+        if (!response.ok) throw new Error('Network response was not ok');
+
+        return await response.json();
+    } catch (error) {
+        console.error('An error occurred:', error.message);
+        throw error;
+    }
+}
