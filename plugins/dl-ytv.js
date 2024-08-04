903d564593960e839e1ea92ccb59fd6e683ec768
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-08-04 19:01:58 -0400

    Actualizar dl-ytv.js

--- plugins/dl-ytv.js
+++ plugins/dl-ytv.js
@@ -11,7 +11,7 @@ if (!args || !args[0]) return conn.reply(m.chat, `*🚩 Escribe la URL de un vid
 if (!args[0].match(/youtu/gi)) return conn.reply(m.chat,`Verifica que la *URL* sea de YouTube`, m).then(_ => m.react('✖️'))
 let q = args[1] || '360p'
 
-await m.react('📀')
+await m.react('🕒')
 try {
 const yt = await fg.ytv(args[0], q)
 let { title, dl_url, size } = yt 
@@ -19,16 +19,10 @@ let vid = (await yts(text)).all[0]
 
 if (size.split('MB')[0] >= limit) return conn.reply(m.chat, `El archivo pesa mas de ${limit} MB, se canceló la Descarga.`, m).then(_ => m.react('✖️'))
 
-await conn.sendMessage(m.chat, {
-        text: `📀 *_CARGANDO..._*\n🍒 *_ESPERA_* @${m.sender.split`@`[0]}`,
-        contextInfo: { 
-          mentionedJid: [m.sender],
-        }
-      }, { quoted: fkontak })
-
-await conn.sendFile(m.chat, dl_url, 'yt.jpg', `${vid.title}\n⇆ㅤㅤ◁ㅤㅤ❚❚ㅤㅤ▷ㅤㅤ↻\n01:26 ━━━━●────── ${vid.timestamp}`, m)
 await m.react('✅')
+await conn.sendFile(m.chat, dl_url, 'yt.jpg', `${vid.title}\n⇆ㅤㅤ◁ㅤㅤ❚❚ㅤㅤ▷ㅤㅤ↻\n01:26 ━━━━●────── ${vid.timestamp}`, m)
 } catch {
+await m.react('🕒')
 try {
 let yt = await fg.ytmp4(args[0], q)
 let { title, size, dl_url } = yt
@@ -36,15 +30,8 @@ let vid = (await yts(text)).all[0]
 
 if (size.split('MB')[0] >= limit) return conn.reply(m.chat, `El archivo pesa mas de ${limit} MB, se canceló la Descarga.`, m).then(_ => m.react('✖️'))
 
-await conn.sendMessage(m.chat, {
-        text: `📀 *_CARGANDO..._*\n🍒 *_ESPERA_* @${m.sender.split`@`[0]}`,
-        contextInfo: { 
-          mentionedJid: [m.sender],
-        }
-      }, { quoted: fkontak })
-
-await conn.sendFile(m.chat, dl_url, 'yt.jpg', `${vid.title}\n⇆ㅤㅤ◁ㅤㅤ❚❚ㅤㅤ▷ㅤㅤ↻\n00:15 ━━━━●────── ${vid.timestamp}`, m)
 await m.react('✅')
+await conn.sendFile(m.chat, dl_url, 'yt.jpg', `${vid.title}\n⇆ㅤㅤ◁ㅤㅤ❚❚ㅤㅤ▷ㅤㅤ↻\n00:15 ━━━━●────── ${vid.timestamp}`, m)
 } catch {
 await conn.reply(m.chat,`*☓ Ocurrió un error inesperado*`, m).then(_ => m.react('✖️'))
 //console.error(error)
