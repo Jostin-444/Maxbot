1a97bb51cd75da1b69175cfbc556fcca11b9e200
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-08-06 14:25:26 -0400

    Actualizar Prueba6.js

--- plugins/Prueba6.js
+++ plugins/Prueba6.js
@@ -5,6 +5,6 @@ let str = `${wm}`
 await conn.sendButton(m.chat, `᥀·࣭࣪̇˖🪐◗ ¿𝘾𝙊𝙈𝙊 𝘿𝙀𝙎𝙀𝘼 𝙑𝙄𝙉𝘾𝙐𝙇𝘼𝙍?\n\n• 𝙎𝙚𝙡𝙚𝙘𝙘𝙞𝙤𝙣𝙚 𝙨𝙤𝙡𝙤 𝙪𝙣𝙖 𝙤𝙥𝙘𝙞𝙤𝙣.\n`, str, media, [
 ['𝗤𝗥 🤖', '.serbotqr'],
 ['𝗖𝗢𝗗𝗘 🤖', '.serbotcode']], null, [
-['Creador', `https://wa.me/5493873232212`]], fkontak)}
+['Creador', `https://wa.me/59168683798`]], fkontak)}
 handler.command = /^serbot|jadibot$/i
 export default handler
