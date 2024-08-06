22be7c77feb5efd8ff77cc0751c171cbde876cc3
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-08-06 19:04:53 -0400

    Actualizar Prueba4.js

--- plugins/Prueba4.js
+++ plugins/Prueba4.js
@@ -7,16 +7,16 @@ if (args.length >= 1) {
 text = args.slice(0).join(' ')
 } else if (m.quoted && m.quoted.text) {
 text = m.quoted.text
-} else return conn.reply(m.chat, `『✦』Ingresa un texto para crear una imagen con dall-e.`, m, fpay, )
+} else return conn.reply(m.chat, `『✦』Ingresa un texto para crear una imagen con dall-e.`, m, fake, )
 
 try {
 
 conn.reply(m.chat, '『✦』Creando imagen, espere un momento...', m, fpay, )
 await Draw(text).then((img) => {
-conn.sendFile(m.chat, img, text, `『✦』 ${text}\n\n¡¡¡Realizado!!!`, m, fpay, )
+conn.sendFile(m.chat, img, text, `『✦』 ${text}\n\n¡¡¡Realizado!!!`, m, fake, )
 })
 } catch (e) {
-return conn.reply(m.chat, `『✦』 *Ocurrio un error inesperado en el comando.*`, m, fpay, )
+return conn.reply(m.chat, `『✦』 *Ocurrio un error inesperado en el comando.*`, m, fake, )
 }
 
 }
