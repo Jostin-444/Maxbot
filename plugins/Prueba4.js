98887d06a5917d57808300080ee72113720deb94
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-08-06 19:05:41 -0400

    Actualizar Prueba4.js

--- plugins/Prueba4.js
+++ plugins/Prueba4.js
@@ -1,44 +1,40 @@
 import fetch from 'node-fetch'
 
-var handler = async (m, { conn, isOwner, usedPrefix, command, args }) => {
+var handler = async (m, {conn, text, usedPrefix, command}) => {
 
-let text
-if (args.length >= 1) {
-text = args.slice(0).join(' ')
-} else if (m.quoted && m.quoted.text) {
-text = m.quoted.text
-} else return conn.reply(m.chat, `『✦』Ingresa un texto para crear una imagen con dall-e.`, m, fake, )
+if (!text) return conn.reply(m.chat, `『✦』Ingrese un texto para crear una imagen con dall-e 2.`, m, fpay, )
+
+conn.reply(m.chat, '『✦』Creando imagen...', m)
 
 try {
 
-conn.reply(m.chat, '『✦』Creando imagen, espere un momento...', m, fpay, )
-await Draw(text).then((img) => {
-conn.sendFile(m.chat, img, text, `『✦』 ${text}\n\n¡¡¡Realizado!!!`, m, fake, )
-})
-} catch (e) {
-return conn.reply(m.chat, `『✦』 *Ocurrio un error inesperado en el comando.*`, m, fake, )
-}
+let res1 = await fetch(`https://vihangayt.me/tools/imagine?q=${text}`)
+let json1 = await res1.json();
+await conn.sendMessage(m.chat, {image: {url: json1.data}}, {quoted: m})
+} catch {
+console.log('『✦』Ocurrio un error en la api 1.')
+try {
+let res2 = await conn.getFile(`https://vihangayt.me/tools/midjourney?q=${text}`)
+await conn.sendMessage(m.chat, {image: {url: res2.data}}, {quoted: m})
+} catch {
+console.log('『✦』Ocurrio un error en la api 2.');
+try {
+let res3 = await fetch(`https://vihangayt.me/tools/lexicaart?q=${text}`)
+let json3 = await res3.json()
+await conn.sendMessage(m.chat, {image: {url: json3.data[0].images[0].url}}, {quoted: m})
+} catch {
+console.log('『✦』Ocurrio un error en la api 3.')
+try {
+const tiores4 = await conn.getFile(`https://api.lolhuman.xyz/api/dall-e?apikey=${lolkeysapi}&text=${text}`)
+await conn.sendMessage(m.chat, {image: {url: tiores4.data}}, {quoted: m})
+} catch {
+console.log('『✦』Ocurrio un error inesperado, no funciono ninguna API...')
+return conn.reply(m.chat, `『✦』Error, el comando fallo.`, m )
+}}}}
 
 }
-handler.help = ['dalle']
+handler.help = ['dall2']
 handler.tags = ['ai']
-handler.command = /^(dalle|openiamage|aiimage|aiimg|aimage|iaimagen|openaimage|openaiimage)/i
+handler.command = /^(dall2)/i
 
-export default handler 
-
-async function Draw(propmt) {
-const Blobs = await fetch(
-'https://api-inference.huggingface.co/models/prompthero/openjourney-v2',
-{
-method: 'POST',
-headers: {
-'content-type': 'application/json',
-Authorization: 'Bearer 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',
-},
-body: JSON.stringify({ inputs: propmt }),
-})
-.then((res) => res.blob())
-const arrayBuffer = await Blobs.arrayBuffer()
-const buffer = Buffer.from(arrayBuffer)
-return buffer
-}
+export default handler
