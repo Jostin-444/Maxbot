c66ecf05fa8caa39e12c0b38944eb00686705a8f
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-08-06 15:15:46 -0400

    Actualizar Prueba3.js

--- plugins/Prueba3.js
+++ plugins/Prueba3.js
@@ -8,6 +8,8 @@ let str = `
 `
 await conn.sendButton(m.chat, str, `𝆭  𝆺𝅥 🪐𖡹⃢⃟ᗒᵉ𝙀ִ𝙭𝆭𝙤ִ𝙩𝙞𝙘𝆭𝙤ִ ִ𝘽ִ𝆭𝙤𝙩 ִ𝙈ִ𝆭𝘿ᵥᗕ⃢⃟𖡹̤🪐 𝆹𝅥 𝆭`, media,
 [['𝙋𝙤𝙧 𝙏𝙚𝙧𝙢𝙪𝙭', '.txgatabot'], ['𝙋𝙤𝙧 𝘾𝙡𝙤𝙪𝙙 𝙎𝙝𝙚𝙡𝙡', '/csgatabot']], null, [['𝘎𝘢𝘵𝘢𝘉𝘰𝘵-𝘔𝘋', `https://github.com/GataNina-Li/GataBot-MD`]], fkontak)}
+handler.help = ['stallgatabot']
+handler.tags = ['Bots']
 handler.command = /^stallgatabot$/i
 handler.exp = 33
 export default handler
