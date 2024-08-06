84c6664a6b0f3f1e6a8668b8d23852427aa4f326
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-08-06 15:49:25 -0400

    Actualizar rg-verificar.js

--- plugins/rg-verificar.js
+++ plugins/rg-verificar.js
@@ -28,7 +28,7 @@ let handler = async function (m, { conn, text, usedPrefix, command }) {
       txt += `*│ .・゜゜・・゜゜・．*\n`
       txt += `*│* 🐈‍⬛ *Nombre* ${name}\n`
       txt += `*│* 🐈‍⬛ *Edad* ${age} años\n`
-      txt += `*│* 🐈‍⬛ *fecha* ${fecha} años\n`
+      txt += `*│* 🐈‍⬛ *fecha* ${fecha}\n`
       txt += `*│* 🐈‍⬛ *Número de serie* \`${sn}\`\n`
       txt += `*╰⪽⫘⫘⫘⫘⫘⫘⫘⫘⫘⪾*`
 await conn.sendLuffy(m.chat, botname, textbot, txt, img, img, canal, m)
