8d34bd63c1dbb8d4dd8efec8c66a5fd9d53421b3
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-08-06 18:52:00 -0400

    Actualizar index.js

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ font: 'chrome',
 align: 'center',
 colors: ['red']
 });
-say(`Developed By • OfcDiego`, {
+say(`By • Angel-OFC y Titanium Team`, {
 font: 'console',
 align: 'center',
 colors: ['yellow']
