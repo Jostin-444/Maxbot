0e9fee8a28b922d36fa21a9c925eefb1d4007ac0
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-08-06 15:27:44 -0400

    Actualizar Prueba7.js

--- plugins/Prueba7.js
+++
@@ -1,8 +0,0 @@
-`Antes de comenzar a utilizar el bot, no se olvide de *verificarse o registrarse* para usar todos los comandos libremente👇
-`
-await conn.sendButton(m.chat, menu, wm, imagebot, [
-['𝙈𝙀𝙉𝙐 𝘾𝙊𝙈𝙋𝙇𝙀𝙏𝙊 💥', '.menucompleto'],
-['𝘼𝙐𝙏𝙊 𝙑𝙀𝙍𝙄𝙁𝙄𝘾𝘼𝙍 ✅', '#reg nauSub.20']], null, [
-['𝘾𝘼𝙉𝘼𝙇 𝙊𝙁𝙄𝘾𝙄𝘼𝙇🌎', `${canal}`]], null, null, fkontak)}
-handler.command = /^menu3|help|comandos|memu|menú|memú$/i
-handler.register = false
