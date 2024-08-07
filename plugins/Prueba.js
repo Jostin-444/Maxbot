c3f4fda71ce9937f829c958d9f0dc7a551fc2540
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-08-06 22:24:15 -0400

    Actualizar Prueba.js

--- plugins/Prueba.js
+++ plugins/Prueba.js
@@ -1,20 +1,10 @@
-case 'fotocompartida': {
-sock.sendMessage(from, { react: { text: `💕`, key: info.key }})
-let anu = await fetchJson('https://raw.githubusercontent.com/iamriz7/kopel_/main/kopel.json')
-let random = anu[Math.floor(Math.random() * anu.length)]
-sock.sendMessage(from, { image: { url: random.male }, caption: `*_Foto masculina:_*` }, { quoted: info })
-sock.sendMessage(from, { image: { url: random.female }, caption: `*_Foto feminina:_*` }, { quoted: info })
-}
-break
-
 import axios from "axios"
 let handler = async (m, {command, conn, usedPrefix}) => {
         m.react('✅')
 let anu = (await axios.get(`https://raw.githubusercontent.com/iamriz7/kopel_/main/kopel.json`)).data  
-let random = anu[Math.floor(Math.random() * anu.length)] 
-conn.sendFile(m.chat, haha, 'error.jpg', `🍟 *${command}*`, m, null, rcanal);
-};  
-conn.sendFile(m.chat, haha, 'error.jpg', `🍟 *${command}*`, m, null, rcanal);
+let random = anu[Math.floor(Math.random() * anu.length)]
+conn.sendFile(from, { image: { url: random.male }, caption: `*_Foto masculina:_*` }, { quoted: info })
+conn.sendFile(from, { image: { url: random.female }, caption: `*_Foto feminina:_*` }, { quoted: info })
 };
 handler.command = handler.help = ['takeda', 'asuma', 'endeavor']
 handler.tags = ['nsfw']
