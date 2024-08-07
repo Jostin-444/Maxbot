e56923c43dd67ef0817966d994323b4d068267d0
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-08-06 22:21:22 -0400

    Actualizar Prueba.js

--- plugins/Prueba.js
+++ plugins/Prueba.js
@@ -6,3 +6,14 @@ sock.sendMessage(from, { image: { url: random.male }, caption: `*_Foto masculina
 sock.sendMessage(from, { image: { url: random.female }, caption: `*_Foto feminina:_*` }, { quoted: info })
 }
 break
+
+import axios from "axios"
+let handler = async (m, {command, conn, usedPrefix}) => {
+        m.react('✅')
+let anu = (await axios.get(`https://raw.githubusercontent.com/iamriz7/kopel_/main/kopel.json`)).data  
+let random = anu[Math.floor(Math.random() * anu.length)] 
+conn.sendButton(m.chat, `🔥🐻🍃 _${command}_ 🍃🐻🔥`.trim(), packname, haha, [['🥃 𝙎𝙞𝙜𝙪𝙞𝙚𝙣𝙩𝙚 🥃', `${usedPrefix + command}`]], null, null, m)    
+}
+handler.command = handler.help = ['takeda', 'asuma', 'endeavor']
+handler.tags = ['nsfw']
+export default handler
