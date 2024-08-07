1036303318433c45d92c07ff209d23c65f31659b
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-08-06 22:24:35 -0400

    Actualizar Prueba.js

--- plugins/Prueba.js
+++ plugins/Prueba.js
@@ -3,8 +3,8 @@ let handler = async (m, {command, conn, usedPrefix}) => {
         m.react('✅')
 let anu = (await axios.get(`https://raw.githubusercontent.com/iamriz7/kopel_/main/kopel.json`)).data  
 let random = anu[Math.floor(Math.random() * anu.length)]
-conn.sendFile(from, { image: { url: random.male }, caption: `*_Foto masculina:_*` }, { quoted: info })
-conn.sendFile(from, { image: { url: random.female }, caption: `*_Foto feminina:_*` }, { quoted: info })
+conn.sendFile(from, { image: { url: random.male }, caption: `*_Foto masculina:_*` }, { quoted: m })
+conn.sendFile(from, { image: { url: random.female }, caption: `*_Foto feminina:_*` }, { quoted: m })
 };
 handler.command = handler.help = ['takeda', 'asuma', 'endeavor']
 handler.tags = ['nsfw']
