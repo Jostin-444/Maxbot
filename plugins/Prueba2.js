d48fcdb36fe30e3dedc865ff7e9811fca5330f5c
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-08-06 22:36:01 -0400

    Actualizar Prueba2.js

--- plugins/Prueba2.js
+++ plugins/Prueba2.js
@@ -3,8 +3,8 @@ let handler = async (m, {command, conn, usedPrefix}) => {
         m.react('✅')
 let anu = await fetchJson('https://raw.githubusercontent.com/iamriz7/kopel_/main/kopel.json')
 let random = anu[Math.floor(Math.random() * anu.length)]
-sock.sendMessage(from, { image: { url: random.male }, caption: `*_Foto masculina:_*` }, { quoted: info })
-sock.sendMessage(from, { image: { url: random.female }, caption: `*_Foto feminina:_*` }, { quoted: info })
+conn.sendFile(m.chat, { image: { url: random.male }, caption: `*_Foto masculina:_*` }, { quoted: info })
+conn.sendFile(m.chat, { image: { url: random.female }, caption: `*_Foto feminina:_*` }, { quoted: info })
 }
 handler.command = handler.help = ['fotocompartida2']
 handler.tags = ['nsfw']
