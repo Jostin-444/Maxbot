e89ae623a1e47ad0be9a761fba93655aa9f3671a
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-08-06 22:37:23 -0400

    Actualizar Prueba.js

--- plugins/Prueba.js
+++ plugins/Prueba.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 let handler = async (m, {command, conn, usedPrefix}) => {
         m.react('✅')
-let anu = (await axios.get(`https://raw.githubusercontent.com/iamriz7/kopel_/main/kopel.json`)).data  
+let anu = (await axios.get(`https://raw.githubusercontent.com/iamriz7/kopel_/main/kopel.json`)).data    
 let random = anu[Math.floor(Math.random() * anu.length)]
 conn.sendFile(m.chat, { image: { url: random.male }, caption: `*_Foto masculina:_*` }, { quoted: m })
 conn.sendFile(m.chat, { image: { url: random.female }, caption: `*_Foto feminina:_*` }, { quoted: m })
