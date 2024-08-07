a3e9883e3fc9f1cfd53cdbc421fb4ececadb7955
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-08-06 22:31:47 -0400

    Actualizar Prueba2.js

--- plugins/Prueba2.js
+++ plugins/Prueba2.js
@@ -1,4 +1,4 @@
-case 'fotocompartida': {
+case 'fotocompartida2': {
 sock.sendMessage(from, { react: { text: `💕`, key: info.key }})
 let anu = await fetchJson('https://raw.githubusercontent.com/iamriz7/kopel_/main/kopel.json')
 let random = anu[Math.floor(Math.random() * anu.length)]
