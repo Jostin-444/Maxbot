4d3d99df33d8961a85af1aa9e95a84d4b817b3c9
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-08-09 12:46:17 -0400

    Actualizar Prueba.js

--- plugins/Prueba.js
+++ plugins/Prueba.js
@@ -1,11 +1,62 @@
-import axios from "axios"
-let handler = async (m, {command, conn, usedPrefix}) => {
-        m.react('✅')
-let anu = (await axios.get(`https://raw.githubusercontent.com/iamriz7/kopel_/main/kopel.json`)).data    
-let random = anu[Math.floor(Math.random() * anu.length)]
-conn.sendFile(m.chat, { image: { url: random.male }, caption: `*_Foto masculina:_*` }, { quoted: m })
-conn.sendFile(m.chat, { image: { url: random.female }, caption: `*_Foto feminina:_*` }, { quoted: m })
+let handler = async (m, { conn, args, isAdmin}) => {
+    let who;
+    
+    if (m.quoted && m.quoted.sender) {
+        who = m.quoted.sender;
+    } else if (m.mentionedJid && m.mentionedJid.length > 0) {
+        who = m.mentionedJid[0];
+    } else {
+        throw `Siapa yang ingin Anda pilih?`;
+    }
+
+    const groupOwner = await (await conn.groupMetadata(m.chat)).owner;
+    const tag = await (await conn.groupMetadata(m.chat)).participants.map(a => a.id);
+
+    if (who === m.sender || who === groupOwner || who === conn.user.jid) {
+        throw `Anda tidak dapat memilih pembuat grup atau diri Anda sendiri!`;
+    }
+
+    let user = global.db.data.users[who];
+
+    if (user.lastVotedBy === m.sender) {
+        throw `Anda tidak dapat memilih orang yang sama lagi selama masa pemungutan suara!`;
+    }
+    if (!user.votekick) {
+    user.votekick = 0
+    }
+    user.lastVotedBy = m.sender;
+    user.votekick += 1;
+    
+    let cap = `@${m.sender.split("@")[0]} Telah memilih @${who.split("@")[0]} untuk dihapus!\n*Total Pemungutan Suara:* ${user.votekick}/5`;
+
+    if (user.votekick >= 5) {
+        cap += `\n\nThe user has reached 5 votes and has been removed from the group.`;
+if (!isAdmin) {
+        conn.groupParticipantsUpdate(m.chat, [who], "remove");
+}
+        user.votekick = 0;
+    } else {
+        let timeout = setTimeout(() => {
+            user = global.db.data.users[who];
+            user.lastVotedBy = '';
+            user.votekick = 0;
+            conn.reply(m.chat, `Memberikan suara untuk @${who.split("@")[0]} telah berakhir. Target 5 suara tidak tercapai.`, m, { contextInfo: {
+            mentionedJid: [who]
+            } });
+        }, 5 * 60 * 1000);
+        user.votingTimeout = timeout;
+    }
+    
+    conn.reply(m.chat, cap, null, { contextInfo:
+    {
+     mentionedJid: [m.sender, who] 
+     }});
 };
-handler.command = handler.help = ['fotocompartida']
-handler.tags = ['nsfw']
-export default handler
+
+handler.help = ['vote_kick']
+handler.command = /^(vote_kick|vk|votekick)$/i;
+handler.tags = ['group'];
+handler.group = true;
+handler.botAdmin = true;
+
+export default handler;
