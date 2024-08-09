68856bb378b9c05344406d7364661484a0c8896c
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-08-09 12:56:04 -0400

    Actualizar Prueba.js

--- plugins/Prueba.js
+++ plugins/Prueba.js
@@ -20,11 +20,11 @@ let ress = await axios.get(`https://www.api-nightmares.my.id/api/tiktok-stalk?q=
 let res = ress.data
 let teks = `乂  *STALKER TIKTOK*
 
-◦  Username : ${res.result.username}
-◦  Nickname : ${res.result.name}
-◦  Pengikut  : ${res.result.followers}
-◦  Mengikuti : ${res.result.following}
-◦  Deskripsi  : ${res.result.description}`
+◦  USUARIO : ${res.result.username}
+◦  NOMBRE : ${res.result.name}
+◦  SEGUIDORES : ${res.result.followers}
+◦  SIGUIENDO: ${res.result.following}
+◦  DESCRIPCIÓN : ${res.result.description}`
 await conn.sendMessage(m.chat, {image: {url : res.result.pp_user}, caption: teks}, {quoted: m})
 } catch (err) {
 m.reply('Error Username tidak ditemukan\nSilahkan kirim Username yang valid!')
