df355aeb0eb13922a195ee5b9443255627e37be9
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-08-09 13:19:31 -0400

    Actualizar Prueba6.js

--- plugins/Prueba6.js
+++ plugins/Prueba6.js
@@ -1,146 +1,35 @@
-{
-  "creator": "DIEGO-OFC",
-  "status": true,
-  "data": [
-    {
-      "title": "HISTED - MASHA ULTRAFUNK",
-      "duration": "1:34",
-      "popularity": "79%",
-      "preview": "https://p.scdn.co/mp3-preview/5f1d681f115a98cb6335846258f4f78ccd1ca53f?cid=acc6302297e040aeb6e4ac1fbdfd62c3",
-      "url": "https://open.spotify.com/track/5VZ0soW5syQfefCUj603DW"
-    },
-    {
-      "title": "PlayaPhonk - Phonky Town",
-      "duration": "2:21",
-      "popularity": "66%",
-      "preview": "https://p.scdn.co/mp3-preview/4f7758fdad558231887e833d51dfacfb95efd1b4?cid=acc6302297e040aeb6e4ac1fbdfd62c3",
-      "url": "https://open.spotify.com/track/5mnpMkzp3Z2RCCbwIEZCbv"
-    },
-    {
-      "title": "Eternxlkz - SLAY!",
-      "duration": "1:47",
-      "popularity": "83%",
-      "preview": "https://p.scdn.co/mp3-preview/f873676632570982293f1614dc9df821d4dc6e00?cid=acc6302297e040aeb6e4ac1fbdfd62c3",
-      "url": "https://open.spotify.com/track/67smGwuPEtA6GAfeweAVNO"
-    },
-    {
-      "title": "DJ MOIGUS - PHONK BRASILEIRO FRESCO",
-      "duration": "1:16",
-      "popularity": "62%",
-      "preview": "https://p.scdn.co/mp3-preview/e7fb1ebb3ae7e0d71b540dad2d434c8c9dc0ba59?cid=acc6302297e040aeb6e4ac1fbdfd62c3",
-      "url": "https://open.spotify.com/track/6Y975MrXSsIJaXyvPSKdsQ"
-    },
-    {
-      "title": "LDRR - Mangos - Tiktok Version",
-      "duration": "0:52",
-      "popularity": "73%",
-      "preview": "https://p.scdn.co/mp3-preview/d434de6ffd3f0c32fdd7313ba10751a01c000c8c?cid=acc6302297e040aeb6e4ac1fbdfd62c3",
-      "url": "https://open.spotify.com/track/1wD755PZiaAhiCOgvFLGq6"
-    },
-    {
-      "title": "Funk Tribu - Phonky Tribu",
-      "duration": "4:46",
-      "popularity": "60%",
-      "preview": "https://p.scdn.co/mp3-preview/79e2274b240b6805332d28fcadf30cdfcb822f79?cid=acc6302297e040aeb6e4ac1fbdfd62c3",
-      "url": "https://open.spotify.com/track/7aIb17DMLcOhLJIc9vF6Aa"
-    },
-    {
-      "title": "Dyan Dxddy - CUTE DEPRESSED",
-      "duration": "1:37",
-      "popularity": "80%",
-      "preview": "https://p.scdn.co/mp3-preview/a6a2f1fe7bd0da3e16035978d4a108752af104e0?cid=acc6302297e040aeb6e4ac1fbdfd62c3",
-      "url": "https://open.spotify.com/track/06PqeO7IInus2yvzsZwZhf"
-    },
-    {
-      "title": "Rushex - PHONKGASKAR",
-      "duration": "1:23",
-      "popularity": "58%",
-      "preview": "https://p.scdn.co/mp3-preview/6870f1ce03ab9db387fe1af73d1496144a61ed19?cid=acc6302297e040aeb6e4ac1fbdfd62c3",
-      "url": "https://open.spotify.com/track/7FoanMQjdrrecBH8YiIti2"
-    },
-    {
-      "title": "SXID - Funk of Galáctico",
-      "duration": "1:30",
-      "popularity": "77%",
-      "preview": "https://p.scdn.co/mp3-preview/26a41d8f28b65b569fe13b14e1fd236466581d32?cid=acc6302297e040aeb6e4ac1fbdfd62c3",
-      "url": "https://open.spotify.com/track/6fxeXlCSt4WLVlbqxgUBMB"
-    },
-    {
-      "title": "Suave Lee - Phonky Trap",
-      "duration": "2:31",
-      "popularity": "54%",
-      "preview": "https://p.scdn.co/mp3-preview/89ecf9d3ac753cca9125b77cb46609c552e5bf60?cid=acc6302297e040aeb6e4ac1fbdfd62c3",
-      "url": "https://open.spotify.com/track/7BMQKWEdSZOZj33Tg2hfGm"
-    },
-    {
-      "title": "g3ox_em - GigaChad Theme - Phonk House Version",
-      "duration": "2:26",
-      "popularity": "74%",
-      "preview": "https://p.scdn.co/mp3-preview/6570e81d4c63389444b4cd049c5e4a5fcf7913d8?cid=acc6302297e040aeb6e4ac1fbdfd62c3",
-      "url": "https://open.spotify.com/track/7mLWNwcvwRdEviz6SfYp8A"
-    },
-    {
-      "title": "6YNTHMANE - BRAZILIAN DANÇA PHONK",
-      "duration": "1:58",
-      "popularity": "76%",
-      "preview": "https://p.scdn.co/mp3-preview/6dd573c943c29002310c46f3d87a6dc615a27bbd?cid=acc6302297e040aeb6e4ac1fbdfd62c3",
-      "url": "https://open.spotify.com/track/6yVjjQA31SLM1kc4j5jPwF"
-    },
-    {
-      "title": "ZWE1HVNDXR - LOVELY BASTARDS",
-      "duration": "1:56",
-      "popularity": "78%",
-      "preview": "https://p.scdn.co/mp3-preview/1688f632932eb0d0fcbc429c8d9cde7c63874eb0?cid=acc6302297e040aeb6e4ac1fbdfd62c3",
-      "url": "https://open.spotify.com/track/0EqczV3o6fwksegAavIHpX"
-    },
-    {
-      "title": "NUEKI - Mexican Phonk Eki",
-      "duration": "1:38",
-      "popularity": "70%",
-      "preview": "https://p.scdn.co/mp3-preview/fdea31c007913f3c51f85a2d8b73f04f06a0c8d6?cid=acc6302297e040aeb6e4ac1fbdfd62c3",
-      "url": "https://open.spotify.com/track/586x98q1tY2vlDlhN1BwQM"
-    },
-    {
-      "title": "dawnicy - SONIC.EXE",
-      "duration": "1:25",
-      "popularity": "70%",
-      "preview": "https://p.scdn.co/mp3-preview/0bc458717c745d49dbc548dc0bd957d63c02e517?cid=acc6302297e040aeb6e4ac1fbdfd62c3",
-      "url": "https://open.spotify.com/track/5wwRFT1RLMtlrYxryyMN73"
-    },
-    {
-      "title": "$werve - Phonk Up Brazil",
-      "duration": "1:30",
-      "popularity": "57%",
-      "preview": "https://p.scdn.co/mp3-preview/8b5ba860e83e74c1bd41c49a9d73182efa818cbe?cid=acc6302297e040aeb6e4ac1fbdfd62c3",
-      "url": "https://open.spotify.com/track/4lxLioQ8YilK1QbF7FxGjZ"
-    },
-    {
-      "title": "Dxrk ダーク - Phonk Web",
-      "duration": "1:32",
-      "popularity": "51%",
-      "preview": "https://p.scdn.co/mp3-preview/dfa02956b4f7bcb719aa7a1c8b3649b7d8f0faec?cid=acc6302297e040aeb6e4ac1fbdfd62c3",
-      "url": "https://open.spotify.com/track/6KY3lpaxiNKaZwDJGix41g"
-    },
-    {
-      "title": "Kordhell - Murder In My Mind",
-      "duration": "2:25",
-      "popularity": "78%",
-      "preview": "https://p.scdn.co/mp3-preview/ec3b94db31d9fb9821cfea0f7a4cdc9d2a85e969?cid=acc6302297e040aeb6e4ac1fbdfd62c3",
-      "url": "https://open.spotify.com/track/6qyS9qBy0mEk3qYaH8mPss"
-    },
-    {
-      "title": "Kordhell - Live Another Day",
-      "duration": "2:14",
-      "popularity": "69%",
-      "preview": "https://p.scdn.co/mp3-preview/ecf2cc576b9f49bc75922265e9630584688dba2e?cid=acc6302297e040aeb6e4ac1fbdfd62c3",
-      "url": "https://open.spotify.com/track/34lfu3S4fIcKZRNXzbdbo7"
-    },
-    {
-      "title": "INTERWORLD - METAMORPHOSIS",
-      "duration": "2:23",
-      "popularity": "79%",
-      "preview": "https://p.scdn.co/mp3-preview/61baac6cfee58ae57f9f767a86fa5e99f9797664?cid=acc6302297e040aeb6e4ac1fbdfd62c3",
-      "url": "https://open.spotify.com/track/2ksyzVfU0WJoBpu8otr4pz"
-    }
-  ]
-}
+import fetch from "node-fetch";
+
+let handler = async (m, { conn, usedPrefix, text }) => {
+  if (!text) return conn.reply(m.chat,"*💚 𝙸𝚗𝚐𝚛𝚎𝚜𝚊 𝚕𝚘 𝚚𝚞𝚎 𝚍𝚎𝚜𝚎𝚊𝚜 𝚋𝚞𝚜𝚌𝚊𝚛 𝚎𝚗 𝚂𝚙𝚘𝚝𝚒𝚏𝚢.*", m);
+  await m.react("💚");
+  let results;
+  try {
+    results = await fetch(`https://thepapusteamspotify.koyeb.app/api/spotify/search?q=${encodeURIComponent(text)}`).then(res => res.json());
+  } catch (error) {
+    console.error(error);
+    await m.react("❌");
+    return conn.reply(m.chat,"𝙷𝚞𝚋𝚘 𝚞𝚗 𝚎𝚛𝚛𝚘𝚛 𝚊𝚕 𝚌𝚘𝚗𝚜𝚞𝚕𝚝𝚊𝚛 𝚎𝚗 𝚂𝚙𝚘𝚝𝚒𝚏𝚢.", m);
+  }
+
+  if (!results || !results.data || results.data.tracks.length === 0)
+    return conn.reply(m.chat,"𝙽𝚘 𝚜𝚎 𝚎𝚗𝚌𝚘𝚗𝚝𝚛𝚊𝚛𝚘𝚗 𝚛𝚎𝚜𝚞𝚕𝚝𝚊𝚍𝚘𝚜, 𝚒𝚗𝚝𝚎𝚗𝚝𝚊 𝚌𝚘𝚗 𝚘𝚝𝚛𝚘 𝚝é𝚛𝚖𝚒𝚗𝚘 𝚍𝚎 𝚋ú𝚜𝚚𝚞𝚎𝚍𝚊.", m).then((_) => m.react("❌"));
+
+  let txt = `*Ｓｐｏｔｉｆｙ-Ｓｅａｒｃｈ \n ⇄ Ⅰ<    ⅠⅠ    >Ⅰ   ↻*`;
+  for (let i = 0; i < (results.data.tracks.length >= 10 ? 10 : results.data.tracks.length); i++) {
+    const track = results.data.tracks[i];
+    txt += `\n\n`;
+    txt += `        ❧  *𝚃𝚒𝚝𝚞𝚕𝚘* : ${track.name}\n`;
+    txt += `        ❧  *𝙰𝚛𝚝𝚒𝚜𝚝𝚊* : ${track.artists}\n`;
+    txt += `        ❧  *Á𝚕𝚋𝚞𝚖* : ${track.album}\n`;
+    txt += `        ❧  *𝙻𝚒𝚗𝚔* : ${track.external_urls.spotify}\n`;
+  }
+
+  conn.reply(m.chat, txt, m, rcanal);
+};
+
+handler.help = ["spotifysearch"];
+handler.tags = ["search"];
+handler.command = /^(spotifysearch)$/i;
+
+export default handler;
