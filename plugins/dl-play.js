e310b7d4b286a473aeb76f2e69a9e346e1b2a6ab
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-08-14 22:34:36 -0400

    Actualizar dl-play.js

--- plugins/dl-play.js
+++ plugins/dl-play.js
@@ -14,8 +14,8 @@ let handler = async (m, { conn, command, text, usedPrefix }) => {
 > 🍀 *DURACIÓN:* ${vid.timestamp}
 > 👀 *VISTAS:* ${vid.views.toLocaleString()}`
  await conn.sendButton(m.chat, play, packname, thumbnail, [
-    ['💿 𝗠 𝗨 𝗦 𝗜 𝗖 𝗔', `${usedPrefix}fgmp3 ${url}`],
-    ['📀 𝗩 𝗜 𝗗 𝗘 𝗢 ', `${usedPrefix}fgmp4 ${url}`],
+    ['💿 𝗠 𝗨 𝗦 𝗜 𝗖 𝗔', `${usedPrefix}play5 ${url}`],
+    ['📀 𝗩 𝗜 𝗗 𝗘 𝗢 ', `${usedPrefix}play6 ${url}`],
 ['📁 𝗠 𝗨 𝗦 𝗜 𝗖 𝗔  𝗗 𝗢 𝗖', `${usedPrefix}ytmp3doc ${url}`],
 ['📁 𝗩 𝗜 𝗗 𝗘 𝗢  𝗗 𝗢 𝗖', `${usedPrefix}ytmp4doc ${url}`]
   ], null, [['🐈‍⬛ 𝗖 𝗔 𝗡 𝗔 𝗟', `${canal}`]], m)
