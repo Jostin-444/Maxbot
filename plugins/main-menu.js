a88fef8b9db46ad456fe78d187521fe29f161b19
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-08-15 14:53:35 -0400

    Actualizar main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -59,7 +59,7 @@ const defaultMenu = {
   footer: '╰⸼ ┄ ┄ ┄ ─  ꒰  ׅ୭🌟୧ ׅ ꒱  ┄  ─ ┄ ⸼\n\n',
   after: `> ${dev}`,
 }
-let ppp = 'https://i.ibb.co/7ks6JT4/file.jpg'
+let ppp = 'https://i.ibb.co/fCZscQV/file.jpg'
 let handler = async (m, { conn, usedPrefix: _p, __dirname }) => {
   try {
     let _package = JSON.parse(await promises.readFile(join(__dirname, '../package.json')).catch(_ => ({}))) || {}
