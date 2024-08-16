efc86857ea5a07b900a205618579b4edf31c5c1a
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-08-16 15:20:40 -0400

    Actualizar gc-config.js

--- plugins/gc-config.js
+++ plugins/gc-config.js
@@ -1,6 +1,6 @@
 var handler = async (m, {conn, args, usedPrefix, command}) => {
 const isClose = { 'open': 'not_announcement', 'close': 'announcement', 'abierto': 'not_announcement', 'cerrado': 'announcement', 'abrir': 'not_announcement', 'cerrar': 'announcement', 'desbloquear': 'unlocked', 'bloquear': 'locked' }[(args[0] || '')]
-if (isClose === undefined) { return await conn.sendButton(m.chat, grupobot, '• 𝙎𝙚𝙡𝙚𝙘𝙘𝙞𝙤𝙣𝙖 𝙡𝙖 𝙘𝙤𝙣𝙛𝙞𝙜𝙪𝙧𝙖𝙘𝙞𝙤𝙣.', pp, [
+if (isClose === undefined) { return await conn.sendButton(m.chat, packname, '• 𝙎𝙚𝙡𝙚𝙘𝙘𝙞𝙤𝙣𝙖 𝙡𝙖 𝙘𝙤𝙣𝙛𝙞𝙜𝙪𝙧𝙖𝙘𝙞𝙤𝙣.', pp, [
 ['𝘼𝙗𝙧𝙞𝙧 ✅', '.grupo abrir'],
 ['𝘾𝙚𝙧𝙧𝙖𝙧 ❌', '.grupo cerrar']], null, null, m)
 }
