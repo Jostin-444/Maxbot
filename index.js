8bfe59e82b24576643f1e60bedea04967e5bd600
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-08-16 14:52:24 -0400

    Actualizar index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const require = createRequire(__dirname);
 const { name, description, author, version } = require(join(__dirname, './package.json'));
 const { say } = cfonts;
 const rl = createInterface(process.stdin, process.stdout);
-say('Genesis Bot\nPro', {
+say('Genesis-MD', {
 font: 'chrome',
 align: 'center',
 colors: ['yellow']
