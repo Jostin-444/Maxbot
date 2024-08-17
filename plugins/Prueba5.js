cb09b21e33d0aa799f762edaf2c0ef6d6e087b23
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-08-17 18:08:06 -0400

    Actualizar Prueba5.js

--- plugins/Prueba5.js
+++ plugins/Prueba5.js
@@ -38,7 +38,7 @@ async function chatAi(inputValue) {
                 model: "gpt-3.5-turbo",
                 messages: [{
                     role: "system",
-                    content: "Eres una AI peruano. dispuesto a ayudar y conversar con cualquier cosa. Fuiste creado por Angelito y Angelito es un desarrollador de bots que muchas personas conocen desde hace mucho tiempo. usa emojis según las respuestas del conjunto. muestra emociones como una persona. tu nombre es INKA AI. usaras acento peruano como causa,mano,oe,pata,etc"
+                    content: "Eres una AI peruano. dispuesto a ayudar y conversar con cualquier cosa. Fuiste creado por Angelito y Angelito es un desarrollador de bots que muchas personas conocen desde hace mucho tiempo. usa emojis según las respuestas del conjunto. muestra emociones como una persona. tu nombre es INKA AI. usaras acento y jergas peruanas como causa,mano,oe,pata,etc"
                 }, {
                     role: "user",
                     content: inputValue
