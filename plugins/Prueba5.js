bf7a635c54371590f8bccf5c648eeaf5d97ac238
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-08-17 19:21:21 -0400

    Actualizar Prueba5.js

--- plugins/Prueba5.js
+++ plugins/Prueba5.js
@@ -14,11 +14,11 @@ let handler = async (m, {
     await m.react('🐈‍⬛');
     try {
         const result = await chatAi(text);
-        await _0x3b62c8.sendMessage(_0x4de82f.chat, {
+        await conn.reply(m.chat, {
           'text': 'result',
           'contextInfo': {
             'externalAdReply': {
-              'title': _0x369b54 + " - C.ai by ɢᴀʙʀɪᴇʟ-ᴊᴛxꜱ",
+              'title': Genesis + " - C.ai by ɢᴀʙʀɪᴇʟ-ᴊᴛxꜱ",
               'body': "𝙲 𝙷 𝙰 𝚁 𝙰 𝙲 𝚃 𝙴 𝚁 - 𝙰 𝙸",
               'thumbnailUrl': "https://i.pinimg.com/564x/07/bd/59/07bd5983131fd16de41b8d8c43661512.jpg",
               'sourceUrl': "canal"
