00c3a86f486a1b1f0894908d508bb3031d23a0d9
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-08-16 22:40:04 -0400

    Actualizar plugins

---
+++ plugins/Prueba3.js
@@ -0,0 +1,53 @@
+// créditos para MauroAzcurra
+// codigo adaptado por karim-off
+import fetch from 'node-fetch';
+
+let handler = async (m, {
+    conn,
+    args,
+    usedPrefix,
+    text,
+    command
+}) => {
+
+ if (!text) throw '➤ `𝗔𝗩𝗜𝗦𝗢` 🐈‍⬛\n\n*PARA USAR GENESIS IA*\n_Ejemplo: .genesis que sos?_';
+    await m.react('🐈‍⬛');
+    try {
+        const result = await chatAi(text);
+        await m.reply(result);
+    } catch (error) {
+        await m.react('😅');
+    }
+
+}
+handler.help = ["genesis"]
+handler.tags = ["ai"];
+handler.command = /^(chatAi|genesis)$/i
+export default handler
+
+async function chatAi(inputValue) {
+    try {
+        const chatApiUrl = 'https://api.chatanywhere.com.cn/v1/chat/completions';
+        const chatResponse = await fetch(chatApiUrl, {
+            method: 'POST',
+            headers: {
+                'Authorization': 'Bearer sk-pu4PasDkEf284PIbVr1r5jn9rlvbAJESZGpPbK7OFYYR6m9g',
+                'Content-Type': 'application/json;charset=UTF-8',
+            },
+            body: JSON.stringify({
+                model: "gpt-3.5-turbo",
+                messages: [{
+                    role: "system",
+                    content: "Eres un asistente. dispuesto a ayudar con cualquier cosa. Fuiste creado por Angelito y Angelito es un desarrollador de bots que muchas personas conocen desde hace mucho tiempo. usa emojis según las respuestas del conjunto"
+                }, {
+                    role: "user",
+                    content: inputValue
+                }]
+            }),
+        });
+        const chatData = await chatResponse.json();
+        return chatData.choices[0].message.content;
+    } catch (error) {
+        throw error;
+    }
+}
