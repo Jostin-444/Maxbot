ee31d1d93c2ec6bc204b2018a475747c656f63ef
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-08-16 22:10:15 -0400

    Actualizar Prueba.js

--- plugins/Prueba.js
+++ plugins/Prueba.js
@@ -1,23 +1,22 @@
 let handler = async (m, { conn, text, usedPrefix, command }) => {
     if (!text) throw(`Contoh:\n${usedPrefix}${command} Halo?`);   
-  let ouh = await fetch(`https://apigratis.site/api/search_characters?query=Hatsune%20Miku`)
- //let ouh = await fetch(`https://apigratis.site/api/search_characters?query=Hatsune%20Miku`)
+  let ouh = await fetch(`https://api.yanzbotz.my.id/api/ai/characterai?text=${text}&name=nezuko`)
   let gyh = await ouh.json() 
   await conn.sendMessage(m.chat, {
   text: `${gyh.result}`,
       contextInfo: {
       externalAdReply: {
-        title: 'Hatsume Miku - C.ai',
+        title: 'Nezuko - C.ai',
         body: 'F U R I N A  M U L T I D E V I C E',
-        thumbnailUrl: 'https://telegra.ph/file/1d84cf5157bffd783a2fd.jpg',
+        thumbnailUrl: 'https://telegra.ph/file/fd03a32e284f69a67114c.jpg',
         sourceUrl: 'https://whatsapp.com/channel/0029VaRI1OB2P59cTdJKZh3q',
         mediaType: 1,
         renderLargerThumbnail: true, 
         showAdAttribution: true
       }}
   })}
-handler.command = /^(caimiku)$/i
-handler.help = ['caimiku']
+handler.command = /^(cainezuko)$/i
+handler.help = ['cainezuko']
 handler.tags = ['character-ai']
 handler.premium = false
 
