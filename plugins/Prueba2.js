74dd021fe6e4a124a1e1d739f6231c0631ada110
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-08-16 22:18:12 -0400

    Actualizar Prueba2.js

--- plugins/Prueba2.js
+++ plugins/Prueba2.js
@@ -1,24 +1,45 @@
-let handler = async (m, { conn, text, usedPrefix, command }) => {
-    if (!text) throw(`Contoh:\n${usedPrefix}${command} Halo?`);   
-  let ouh = await fetch(`https://kiicodeofficial.my.id/api/others/cai?query=${text}&apikey=mswreJVZxE`)
- //let ouh = await fetch(`https://api.betabotz.org/api/search/c-ai?prompt=hai%20ai%20siapa%20namamu?&char=HuTao&apikey=8cZTmd8U`)
-  let gyh = await ouh.json() 
-  await conn.sendMessage(m.chat, {
-  text: `${gyh.result}`,
-      contextInfo: {
-      externalAdReply: {
-        title: 'C.ai',
-        body: 'E L A I N A  M U L T I D E V I C E',
-        thumbnailUrl: 'https://telegra.ph/file/6d3139a708c069f2cc5b5.jpg',
-        sourceUrl: 'https://whatsapp.com/channel/0029VaF8RYn9WtC16ecZws0H',
-        mediaType: 1,
-        renderLargerThumbnail: true, 
-        showAdAttribution: true
-      }}
-  })}
-handler.command = /^(cai)$/i
-handler.help = ['cai']
-handler.tags = ['character-ai']
-handler.premium = false
+import axios from 'axios';
+
+let handler = async (m, { text, command, usedPrefix, conn }) => {
+    if (!text) {
+        throw `Fitur pendeteksi Tulisan yang dibuat oleh AI\nContoh:\n${usedPrefix + command} TextGenerateFromAI`;
+    }
+
+    var options = {
+        method: 'POST',
+        url: 'https://tr.deployers.repl.co/zerogpt',
+        headers: {
+            Accept: '*/*',
+            'User-Agent': 'Thunder Client (https://www.thunderclient.com)',
+            'content-type': 'multipart/form-data; boundary=---011000010111000001101001'
+        },
+        data: `-----011000010111000001101001\r\nContent-Disposition: form-data; name="text"\r\n\r\n${text}\r\n-----011000010111000001101001--\r\n`
+    };
+
+    try {
+        const response = await axios.request(options);
+        const aiWords = response.data.data.aiWords;
+        const detectedLanguage = response.data.data.detected_language;
+        const h = response.data.data.h;
+
+        const replyMessage = `
+AI Words: ${aiWords}
+Detected Language: ${detectedLanguage}
+Perkataan AI: ${h}
+
+Support me on https://tr.deployers.repl.co/images
+`;
+
+        conn.reply(m.chat, replyMessage, m);
+    } catch (error) {
+        console.error(error);
+        conn.reply(m.chat, 'An error occurred while processing the request', m);
+    }
+};
+
+handler.command = /^zerogpt/i;
+handler.help = ['zerogpt <text>'];
+handler.tags = ['tools'];
 
 export default handler;
+          
