74124872c17f042ff3811263f4c1b0daa73cdf66
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-08-17 19:11:51 -0400

    Actualizar Prueba.js

--- plugins/Prueba.js
+++ plugins/Prueba.js
@@ -1,80 +1,81 @@
-import _0x3ea954 from 'node-fetch';
-const translateGoogle = async (_0xe05127, _0xf4d03c, _0x46479e) => {
+import _0x4ba8d8 from 'node-fetch';
+const translateGoogle = async (_0xb0e5a1, _0x54f0a3, _0x49a6b6) => {
   try {
-    const _0x57f1e2 = await _0x3ea954("https://translate.googleapis.com/translate_a/single?client=gtx&sl=" + _0xf4d03c + "&tl=" + _0x46479e + "&dt=t&q=" + encodeURIComponent(_0xe05127));
-    const _0xf85c39 = await _0x57f1e2.json();
-    return _0xf85c39[0][0][0];
-  } catch (_0x2e92fd) {
-    throw new Error("Error en la traducción: " + _0x2e92fd);
+    const _0x3777b3 = await _0x4ba8d8("https://translate.googleapis.com/translate_a/single?client=gtx&sl=" + _0x54f0a3 + "&tl=" + _0x49a6b6 + "&dt=t&q=" + encodeURIComponent(_0xb0e5a1));
+    const _0xd9e58e = await _0x3777b3.json();
+    return _0xd9e58e[0][0][0];
+  } catch (_0x2e7535) {
+    throw new Error("Error en la traducción: " + _0x2e7535);
   }
 };
 const commandMapping = {
-  'emilia': "emilia"
+  'kurumi': "kurumi"
 };
-let handler = async (_0x158ca0, {
-  conn: _0x3d9738,
-  text: _0x4dbdfe
+let handler = async (_0x4de82f, {
+  conn: _0x3b62c8,
+  text: _0x2f9c22
 }) => {
-  if (!_0x4dbdfe) {
-    throw "Ingresa un texto para hablar con Emilia :3";
+  if (!_0x2f9c22) {
+    throw "Ingresa un texto para hablar con kurumi :3";
   }
-  let _0x35112e;
+  let _0x435b29;
   try {
-    _0x35112e = await translateGoogle("emilia", 'es', 'en');
-  } catch (_0x2fd55d) {
-    throw new Error("Error al traducir el comando: " + _0x2fd55d);
+    _0x435b29 = await translateGoogle("kurumi", 'es', 'en');
+  } catch (_0x29d97b) {
+    throw new Error("Error al traducir el comando: " + _0x29d97b);
   }
-  let _0x5afc1b = commandMapping[_0x35112e.toLowerCase()];
-  if (!_0x5afc1b) {
-    throw "El comando '" + _0x35112e + "' no está soportado.";
+  let _0x38f7a9 = commandMapping[_0x435b29.toLowerCase()];
+  if (!_0x38f7a9) {
+    throw "El comando '" + _0x435b29 + "' no está soportado.";
   }
   try {
-    const _0x4496af = await _0x3ea954("https://api.apigratis.site/cai/send_message", {
+    const _0x18f52e = await _0x4ba8d8("https://api.apigratis.site/cai/send_message", {
       'method': "POST",
       'headers': {
         'Content-Type': "application/json"
       },
       'body': JSON.stringify({
-        'external_id': "uayvM9psOOgug10thtXgBnM8OeSdbcJt7gE3aVWSeBk",
-        'message': _0x4dbdfe.trim()
+        'external_id': "jNFgkSl-JTLsDM4d_twATPlFqLkQU-Odmr6h23_d1Jg",
+        'message': _0x2f9c22.trim()
       })
     });
-    if (!_0x4496af.ok) {
-      throw new Error("HTTP error! Status: " + _0x4496af.status);
+    if (!_0x18f52e.ok) {
+      throw new Error("HTTP error! Status: " + _0x18f52e.status);
     }
-    const _0x334410 = await _0x4496af.json();
-    if (_0x334410.status && _0x334410.result && _0x334410.result.state === "STATE_OK") {
+    const _0x33093e = await _0x18f52e.json();
+    if (_0x33093e.status && _0x33093e.result && _0x33093e.result.state === "STATE_OK") {
       const {
-        replies: _0x314426,
-        character_info: _0x37088b
-      } = _0x334410.result;
+        replies: _0x33a29b,
+        character_info: _0x2e28cf
+      } = _0x33093e.result;
       const {
-        name: _0x45ca69
-      } = _0x37088b;
-      for (const _0x28fbf5 of _0x314426) {
-        let _0x53cbe6 = await translateGoogle(_0x28fbf5.text, 'en', 'es');
-        await _0x3d9738.sendMessage(_0x158ca0.chat, {
-          'text': '*' + _0x5afc1b + ":* " + _0x53cbe6,
+        name: _0x369b54
+      } = _0x2e28cf;
+      for (const _0x5c5a92 of _0x33a29b) {
+        let _0x2edc21 = await translateGoogle(_0x5c5a92.text, 'en', 'es');
+        await _0x3b62c8.sendMessage(_0x4de82f.chat, {
+          'text': '*' + _0x38f7a9 + ":* " + _0x2edc21,
           'contextInfo': {
             'externalAdReply': {
-              'title': _0x45ca69 + " - C.ai",
+              'title': _0x369b54 + " - C.ai by ɢᴀʙʀɪᴇʟ-ᴊᴛxꜱ",
               'body': "𝙲 𝙷 𝙰 𝚁 𝙰 𝙲 𝚃 𝙴 𝚁 - 𝙰 𝙸",
-              'thumbnailUrl': "https://i.pinimg.com/564x/b8/9b/3c/b89b3c4140b832cb35832781cd0f3f91.jpg",
+              'thumbnailUrl': "https://i.pinimg.com/564x/07/bd/59/07bd5983131fd16de41b8d8c43661512.jpg",
               'sourceUrl': "canal"
             }
           }
         }, {
-          'quoted': _0x158ca0
+          'quoted': _0x4de82f
         });
       }
     } else {
       throw "Error al procesar la solicitud";
     }
-  } catch (_0x5be2f3) {
-    throw new Error("Error al enviar el mensaje: " + _0x5be2f3);
+  } catch (_0x1e23db) {
+    throw new Error("Error al enviar el mensaje: " + _0x1e23db);
   }
 };
-handler.help = ["rem <txt>"];
+handler.help = ["kurumi <txt>"];
 handler.tags = ['IA'];
-handler.command = /^(emilia5)$/i;
+handler.command = /^(kurumi5)$/i;
+handler.register = true;
 export default handler;
