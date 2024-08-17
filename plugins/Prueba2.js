d696e39e1b0f833c483df54b63c66236d1b7705a
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-08-17 17:09:34 -0400

    Actualizar Prueba2.js

--- plugins/Prueba2.js
+++ plugins/Prueba2.js
@@ -1,53 +1,53 @@
-//código modificado por karim-off 
+// créditos para MauroAzcurra
+// codigo adaptado por karim-off
+import fetch from 'node-fetch';
 
-import { randomBytes } from "crypto"
-import axios from "axios"
+let handler = async (m, {
+    conn,
+    args,
+    usedPrefix,
+    text,
+    command
+}) => {
 
-let handler = async (m, { conn, text }) => {
-    if (!text) throw '¿Cómo puedo ayudarte hoy?';
+ if (!text) throw '➤ `𝗔𝗩𝗜𝗦𝗢` 🐈‍⬛\n\n*PARA USAR GENESIS IA*\n_Ejemplo: .tanjiro que sos?_';
+    await m.react('🐈‍⬛');
     try {
-        conn.reply(m.chat, m);
-        let data = await chatGpt(text)
-        conn.reply(m.chat, data, m);
-    } catch (err) {
-        m.reply('error cik:/ ' + err);
+        const result = await chatAi(text);
+        await m.reply(result);
+    } catch (error) {
+        await m.react('😅');
     }
+
 }
+handler.help = ["genesis"]
+handler.tags = ["ai"];
+handler.command = /^(chatAi|tanjiro|TANJIRO)$/i
+export default handler
 
-handler.command = handler.help = ['demo'];
-handler.tags = ['ai'];
-handler.limit = 3;
-
-export default handler;
-
-async function chatGpt(query){
-try {
-
-const { id_ }= (await axios.post("https://chat.chatgptdemo.net/new_chat",{user_id: "crqryjoto2h3nlzsg"},{headers:{
-"Content-Type": "application/json",
-
-}})).data
-
-const json = {"question":query,"chat_id": id_,"timestamp":new Date().getTime()}
-
-
-const { data } = await axios.post("https://chat.chatgptdemo.net/chat_api_stream",json,{headers:{
-"Content-Type": "application/json",
-
-}})
-const cek = data.split("data: ")
-
-let res = []
-
-for (let i=1; i < cek.length; i++){
-if (cek[i].trim().length > 0){
-res.push(JSON.parse(cek[i].trim()))
-}}
-
-return res.map((a) => a.choices[0].delta.content).join("")
-
-} catch (error) {
-console.error("Error parsing JSON:",error)
-return 404
-}
+async function chatAi(inputValue) {
+    try {
+        const chatApiUrl = 'https://api.chatanywhere.com.cn/v1/chat/completions';
+        const chatResponse = await fetch(chatApiUrl, {
+            method: 'POST',
+            headers: {
+                'Authorization': 'Bearer sk-pu4PasDkEf284PIbVr1r5jn9rlvbAJESZGpPbK7OFYYR6m9g',
+                'Content-Type': 'application/json;charset=UTF-8',
+            },
+            body: JSON.stringify({
+                model: "gpt-3.5-turbo",
+                messages: [{
+                    role: "system",
+                    content: "Eres un Ia de juegos. dispuesto a jugar con cualquier persona. Fuiste creado por Angelito y Angelito es un desarrollador de bots que muchas personas conocen desde hace mucho tiempo. usa emojis según las respuestas del conjunto. muestra emociones como una persona. tu nombre es Tanjiro AI, y jugaras muchos juegos"
+                }, {
+                    role: "user",
+                    content: inputValue
+                }]
+            }),
+        });
+        const chatData = await chatResponse.json();
+        return chatData.choices[0].message.content;
+    } catch (error) {
+        throw error;
+    }
 }
