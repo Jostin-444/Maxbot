edcc65230ba366443814fa6ac5552a72fa0a0a7b
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-08-18 19:16:36 -0400

    Actualizar dl-tiktok.js

--- plugins/dl-tiktok.js
+++ plugins/dl-tiktok.js
@@ -1,2 +1,38 @@
-//código incriptado por Angelito-OFC 
-const _0x4abacd=_0x5aa6;(function(_0x1b54f4,_0x2d97c2){const _0x1fbe9f=_0x5aa6,_0x17a129=_0x1b54f4();while(!![]){try{const _0x5831c7=-parseInt(_0x1fbe9f(0x1c1))/0x1+parseInt(_0x1fbe9f(0x1c0))/0x2+-parseInt(_0x1fbe9f(0x1bc))/0x3*(parseInt(_0x1fbe9f(0x1c4))/0x4)+parseInt(_0x1fbe9f(0x1bf))/0x5+parseInt(_0x1fbe9f(0x1c2))/0x6+parseInt(_0x1fbe9f(0x1c3))/0x7+parseInt(_0x1fbe9f(0x1b8))/0x8;if(_0x5831c7===_0x2d97c2)break;else _0x17a129['push'](_0x17a129['shift']());}catch(_0xf43d7e){_0x17a129['push'](_0x17a129['shift']());}}}(_0x1d8a,0x9d412));function hi(){const _0x1341a0=_0x5aa6;console[_0x1341a0(0x1b4)](_0x1341a0(0x1b7));}function _0x5aa6(_0x1c0a0d,_0x2134b3){const _0x1d8a06=_0x1d8a();return _0x5aa6=function(_0x5aa65a,_0x4d4f8){_0x5aa65a=_0x5aa65a-0x1b4;let _0x5df7c3=_0x1d8a06[_0x5aa65a];return _0x5df7c3;},_0x5aa6(_0x1c0a0d,_0x2134b3);}import{ttdl}from'ruhend-scraper';let handler=async(_0x11dabc,{conn:_0x19714a,args:_0x8ca589,usedPrefix:_0x44dae5,command:_0x7b4ce5})=>{const _0x1c950c=_0x5aa6;if(!_0x8ca589||!_0x8ca589[0x0])return _0x19714a[_0x1c950c(0x1ba)](_0x11dabc[_0x1c950c(0x1bb)],_0x1c950c(0x1be),_0x11dabc,fake);try{await _0x11dabc[_0x1c950c(0x1b5)]('🕓');let {title:_0x511413,author:_0x54fe30,username:_0x301431,published:_0x20aeff,like:_0x5aaca4,comment:_0xfcd7b3,share:_0x2b8e41,views:_0x12b5bf,bookmark:_0x2fecee,video:_0x402d79,cover:_0x323b27,duration:_0x1f9712,music:_0xa588,profilePicture:_0x554aec}=await ttdl(_0x8ca589[0x0]),_0x3136df='';_0x3136df+='',await _0x19714a[_0x1c950c(0x1c5)](_0x11dabc[_0x1c950c(0x1bb)],_0x402d79,_0x1c950c(0x1b6),dev,_0x11dabc),await _0x11dabc[_0x1c950c(0x1b5)]('✅');}catch{await _0x11dabc['react']('❌');}};handler['help']=[_0x4abacd(0x1b9)],handler['tags']=['dl'],handler[_0x4abacd(0x1bd)]=/^(tiktok|ttdl|tiktokdl|tiktoknowm)$/i;function _0x1d8a(){const _0xec500c=['command','➤\x20`𝗔𝗩𝗜𝗦𝗢`\x20🐈‍⬛\x0a\x0a*INGRESA\x20EL\x20LINK\x20DE\x20TIKTOK.*\x0a_Ejemplo:\x20.tiktok\x20<link>_','1965475AVIqDR','306858PKcNqh','270680lvfOLT','3742902LSaqZZ','3332742XiezEx','1241564sydmJh','sendFile','log','react','tiktok.mp4','Hello\x20World!','1596160aGPuPs','tiktok\x20*<link>*','reply','chat','9vOjVJZ'];_0x1d8a=function(){return _0xec500c;};return _0x1d8a();}export default handler;
+import { ttdl } from 'ruhend-scraper';
+
+let handler = async (m, { conn, args, usedPrefix, command }) => {
+ if (!args || !args[0]) return conn.reply(m.chat, '*`INGRESA EL LINK DE TIKTOK`*', m, fake, )
+    try {
+await m.react('🕓'); 
+        let {
+            title,
+            author,
+            username,
+            published,
+            like,
+            comment,
+            share,
+            views,
+            bookmark,
+            video,
+            cover,
+            duration,
+            music,
+            profilePicture
+        } = await ttdl(args[0]);
+
+        let txt = '';
+        txt += ``;
+
+        await conn.sendFile(m.chat, video, 'tiktok.mp4', dev, m);
+        await m.react('✅'); 
+    } catch {
+        await m.react('❌'); 
+    }
+};
+
+handler.help = ['tiktok *<link>*']
+handler.tags = ['dl'] 
+handler.command = /^(tiktok|ttdl|tiktokdl|tiktoknowm)$/i;
+
+export default handler;
