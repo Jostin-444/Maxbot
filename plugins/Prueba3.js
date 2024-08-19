25c7ef5a81d39c26ad71eba804db7d45dbf53353
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-08-19 13:59:57 -0400

    Actualizar Prueba3.js

--- plugins/Prueba3.js
+++ plugins/Prueba3.js
@@ -14,7 +14,19 @@ let handler = async (m, {
     await m.react('🤍');
     try {
         const result = await chatAi(text);
-        await m.reply(result);
+await conn.sendMessage(m.chat, { text: result,
+contextInfo:{
+forwardingScore: 9999999,
+isForwarded: false,
+"externalAdReply": {
+"showAdAttribution": true,
+"containsAutoReply": true,
+title: `[𝗚 𝗘 𝗡 𝗘 𝗦 𝗜 𝗦 - 𝗔 𝗜]`,
+body: `By: Angel-OFC 🐦‍🔥`,
+"previewType": "PHOTO",
+thumbnailUrl: 'https://tinyurl.com/257eot9c', 
+sourceUrl: 'https://whatsapp.com/channel/0029VaJxgcB0bIdvuOwKTM2Y'}}},
+{ quoted: m})
     } catch (error) {
         await m.react('😓');
     }
@@ -38,7 +50,7 @@ async function chatAi(inputValue) {
                 model: "gpt-3.5-turbo",
                 messages: [{
                     role: "system",
-                    content: "Eres una asistente. dispuesto a ayudar con cualquier cosa. Fuiste creado por Angelito y Angelito es un desarrollador de bots que muchas personas conocen desde hace mucho tiempo. usa emojis según las respuestas del conjunto. muestra emociones como una persona. tu nombre es Génesis AI"
+                    content: "Eres una asistente. dispuesto a ayudar con cualquier cosa. Fuiste creado por Angelito y Angelito es un desarrollador de bots que muchas personas conocen desde hace mucho tiempo. usa emojis según las respuestas del conjunto. muestra emociones como una persona. tu nombre es Génesis AI. seras amable y cariñosa. seras muy cariñosa"
                 }, {
                     role: "user",
                     content: inputValue
