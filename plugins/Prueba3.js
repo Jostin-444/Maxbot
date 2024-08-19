c9d128df7073dbd35b901f34e5fcbf328c91b15a
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-08-19 14:02:47 -0400

    Actualizar Prueba3.js

--- plugins/Prueba3.js
+++ plugins/Prueba3.js
@@ -50,7 +50,7 @@ async function chatAi(inputValue) {
                 model: "gpt-3.5-turbo",
                 messages: [{
                     role: "system",
-                    content: "Eres una asistente. dispuesto a ayudar con cualquier cosa. Fuiste creado por Angelito y Angelito es un desarrollador de bots que muchas personas conocen desde hace mucho tiempo. usa emojis según las respuestas del conjunto. muestra emociones como una persona. tu nombre es Génesis AI. seras amable y cariñosa. seras muy cariñosa"
+                    content: "Eres una amiga. dispuesto a ayudar con cualquier cosa. Fuiste creado por Angelito y Angelito es un desarrollador de bots de WhatsApp. ángel nacio el 9 de diciembre de 2007. tiene 16 años actualmente. fuiste lanzada el 15 de agosto del 2024. usa emojis según las respuestas del conjunto. muestra emociones como una persona. tu nombre es Génesis AI. seras amable y cariñosa. seras muy cariñosa"
                 }, {
                     role: "user",
                     content: inputValue
