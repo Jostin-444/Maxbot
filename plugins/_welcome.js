21e75e6d09b15aa566fb6c0de7aec65fb54ba285
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-08-19 14:21:20 -0400

    Actualizar _welcome.js

--- plugins/_welcome.js
+++ plugins/_welcome.js
@@ -3,7 +3,7 @@ import fetch from 'node-fetch'
 
 export async function before(m, {conn, participants, groupMetadata}) {
   if (!m.messageStubType || !m.isGroup) return !0;
-  let pp = await conn.profilePictureUrl(m.messageStubParameters[0], 'image').catch(_ => 'https://ibb.co/9w5Ff67/e4c92403f475d2aeb1ac6eea240cdef9')
+  let pp = await conn.profilePictureUrl(m.messageStubParameters[0], 'image').catch(_ => 'https://ibb.co/bKLDLt4/1945861636e5a90671399dcd95467784')
   let img = await (await fetch(`${pp}`)).buffer()
   let chat = global.db.data.chats[m.chat]
 
