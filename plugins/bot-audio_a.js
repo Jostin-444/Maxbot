5769ad3aeb969b02b04f785bde45c2558aecaa29
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-08-20 19:02:01 -0400

    Actualizar bot-audio_a.js

--- plugins/bot-audio_a.js
+++ plugins/bot-audio_a.js
@@ -2,9 +2,9 @@ const handler = async (m, {conn}) => {
   if (!db.data.chats[m.chat].audios) return;
   if (!db.data.settings[conn.user.jid].audios_bot && !m.isGroup) return;
   //const s = seconds: '1934.4'
-  const vn = './src/mp3/a.mp3';
+  const vn = './media/audios/a.mp3';
   conn.sendPresenceUpdate('recording', m.chat);
-  conn.sendMessage(m.chat, {audio: {url: vn}, ptt: true, mimetype: 'audio/mpeg', fileName: `a.mp3`});
+  conn.sendMessage(m.chat, {audio: {url: vn}, ptt: true, mimetype: 'audio/mpeg', fileName: `a.mp3`}, {quoted: fakegif3});
 };
 handler.customPrefix = /ª|a|A/
 handler.command = /^(a|ª|A?$)/
