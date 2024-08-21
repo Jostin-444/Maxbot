bb3e9f9fbc3f47188b720c25fae01f17365c7d0f
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-08-20 22:42:55 -0400

    Actualizar dl-tiktok.js

--- plugins/dl-tiktok.js
+++ plugins/dl-tiktok.js
@@ -32,7 +32,7 @@ await m.react('🕓');
 };
 
 handler.help = ['tiktok *<link>*']
-handler.corazones = 1
+handler.corazones = 3
 handler.tags = ['dl'] 
 handler.command = /^(tiktok|ttdl|tiktokdl|tiktoknowm)$/i;
 
