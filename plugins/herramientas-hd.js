2cbc346e7cebd71e02b9609f1986f911e75f34a9
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-08-20 20:34:58 -0400

    Actualizar herramientas-hd.js

--- plugins/herramientas-hd.js
+++ plugins/herramientas-hd.js
@@ -4,9 +4,9 @@ const handler = async (m, {conn, usedPrefix, command}) => {
  try {    
   let q = m.quoted ? m.quoted : m;
   let mime = (q.msg || q).mimetype || q.mediaType || "";
-  if (!mime) return m.reply(`🚩 Envie una imagen o responda a la imagen utilizando el comando: ${usedPrefix + command}`);
-  if (!/image\/(jpe?g|png)/.test(mime)) return m.reply(`🍂 El formato del archivo (${mime}) no es compatible, envía o responda a una imagen`);
-  conn.reply(m.chat, '🚩 Mejorando la calidad de la imagen....', m, {
+  if (!mime) return m.reply(`🤍 Envie una imagen o responda a la imagen utilizando el comando: ${usedPrefix + command}`);
+  if (!/image\/(jpe?g|png)/.test(mime)) return m.reply(`🤍 El formato del archivo (${mime}) no es compatible, envía o responda a una imagen`);
+  conn.reply(m.chat, '🤍 Mejorando la calidad de la imagen....', m, {
   contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, showAdAttribution: true,
   title: packname,
   body: wm,
@@ -16,7 +16,7 @@ const handler = async (m, {conn, usedPrefix, command}) => {
   let pr = await remini(img, "enhance");
   conn.sendMessage(m.chat, {image: pr}, {quoted: fkontak});
  } catch {
- return m.reply("🚩 Ocurrió un error");
+ return m.reply("🤍 Ocurrió un error");
  }
 };
 handler.help = ["remini", "hd", "enhance"];
