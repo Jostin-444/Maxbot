2c220f7d68866399c168317327720781019a4dd9
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-08-20 20:32:47 -0400

    Actualizar fun-pokedex.js

--- plugins/fun-pokedex.js
+++ plugins/fun-pokedex.js
@@ -1,9 +1,9 @@
 import fetch from 'node-fetch';
 
 let handler = async (m, { conn, text }) => {
-if (!text) return conn.reply(m.chat, '🚩 *¿Que Pokémon quieres buscar?.*', m, rcanal)
+if (!text) return conn.reply(m.chat, '🤍 *¿Que Pokémon quieres buscar?.*', m, rcanal)
 await m.react(rwait)
-conn.reply(m.chat, `🍟 *Buscando ${text}*`, m, {
+conn.reply(m.chat, `🤍 *Buscando ${text}*`, m, {
 contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, showAdAttribution: true,
 title: packname,
 body: wm,
@@ -14,12 +14,12 @@ const response = await fetch(url);
 const json = await response.json();
 if (!response.ok) {
 await m.react(error)
-return conn.reply(m.chat, '🍟 *¡Oops! Parece que hubo un error al buscar el Pokémon. Por favor, inténtalo de nuevo más tarde.*', m, rcanal)}
-const luffypokedex = `🚩 *Pokedex - Información de ${json.name}*\n\n🍟 *Nombre:* ${json.name}\n🍟 *ID:* ${json.id}\n🍟 *Tipo:* ${json.type}\n🍟 *Habilidades:* ${json.abilities}\n*Tamaño:* ${json.height}\n🍟 *Peso:* ${json.weight}\n\n📖 *Descripción:*\n${json.description}\n\n🔍 ¡Encuentra más detalles sobre este Pokémon en la Pokedex! 🔍\n\n🔗 https://www.pokemon.com/es/pokedex/${json.name.toLowerCase()}`
+return conn.reply(m.chat, '🤍 *¡Oops! Parece que hubo un error al buscar el Pokémon. Por favor, inténtalo de nuevo más tarde.*', m, rcanal)}
+const luffypokedex = `🤍 *Pokedex - Información de ${json.name}*\n\n🤍 *Nombre:* ${json.name}\n🤍 *ID:* ${json.id}\n🤍 *Tipo:* ${json.type}\n🤍 *Habilidades:* ${json.abilities}\n*Tamaño:* ${json.height}\n🤍 *Peso:* ${json.weight}\n\n📖 *Descripción:*\n${json.description}\n\n🔍 ¡Encuentra más detalles sobre este Pokémon en la Pokedex! 🔍\n\n🔗 https://www.pokemon.com/es/pokedex/${json.name.toLowerCase()}`
 conn.reply(m.chat, luffypokedex, m, rcanal)
 await m.react(done) }
 
 handler.help = ['pokedex *<pokemon>*']
-handler.tags = ['fun']
+handler.tags = ['buscador']
 handler.command = /^pokedex/i
 export default handler
