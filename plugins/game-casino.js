744b252f28ba927247db3d1f0167aec970c72aa5
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-08-20 21:15:35 -0400

    Actualizar game-casino.js

--- plugins/game-casino.js
+++ plugins/game-casino.js
@@ -23,12 +23,12 @@ count = count ? /all/i.test(count) ? Math.floor(global.db.data.users[m.sender].l
 count = Math.max(1, count)
 if (args.length < 1) return conn.reply(m.chat, '🤍 Ingresa la cantidad de ' + `🤍 *Corazones*` + ' que deseas aportar contra' + ` *GenesisBot*` + `\n\n` + '`Ejemplo:`\n' + `> *${usedPrefix + command}* 100`, m, rcanal)
 if (user.corazones >= count * 1) {
-user.estrellas -= count * 1
+user.corazones -= count * 1
 if (Aku > Kamu) {
 conn.reply(m.chat, '`🤍 Veamos que numeros tienen!`\n\n'+ `➠ *GenesisBot* : ${Aku}\n➠ *${username}* : ${Kamu}\n\n> ${username}, *PERDISTE* ${formatNumber(count)} 🤍 Corazones.`.trim(), m, rcanal)
 } else if (Aku < Kamu) {
 user.corazones += count * 2
-conn.reply(m.chat, '`🤍 Veamos que numeros tienen!`\n\n'+ `➠ *GenesisBot* : ${Aku}\n➠ *${username}* : ${Kamu}\n\n> ${username}, *GANASTE* ${formatNumber(count * 2)} 🤍 Estrellas.`.trim(), m, rcanal)
+conn.reply(m.chat, '`🤍 Veamos que numeros tienen!`\n\n'+ `➠ *GenesisBot* : ${Aku}\n➠ *${username}* : ${Kamu}\n\n> ${username}, *GANASTE* ${formatNumber(count * 2)} 🤍 Corazones.`.trim(), m, rcanal)
 } else {
 user.corazones += count * 1
 conn.reply(m.chat, '`🤍 Veamos que numeros tienen!`\n\n'+ `➠ *GenesisBot* : ${Aku}\n➠ *${username}* : ${Kamu}\n\n> ${username} obtienes ${formatNumber(count * 1)} 🤍 Corazones.`.trim(), m, rcanal)}
