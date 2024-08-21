6457d1f9b740995028a4a43c12a0d3a1819affc9
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-08-20 21:21:47 -0400

    Actualizar jadibot-deletebot.js

--- plugins/jadibot-deletebot.js
+++ plugins/jadibot-deletebot.js
@@ -9,10 +9,10 @@ let userS = `${conn.getName(who)}`
 
 try {
 await fs.rmdir("./LuffyJadiBot/" + uniqid, { recursive: true, force: true })
-await parentw.sendMessage(m.chat, { text: '🚩 Sub-Bot eliminado.' }, { quoted: fkontak })
+await parentw.sendMessage(m.chat, { text: '☁️ Sub-Bot eliminado.' }, { quoted: fkontak })
 } catch(err) {
 if (err.code === 'ENOENT' && err.path === `./LuffyJadiBot/${uniqid}`) {
-await parentw.sendMessage(m.chat, { text: "🍟 No cuentas con ninguna sesión de Sub-Bot." }, { quoted: fkontak })
+await parentw.sendMessage(m.chat, { text: "🤍 No cuentas con ninguna sesión de Sub-Bot." }, { quoted: fkontak })
 } else {
 await m.react(error)
 }}}
