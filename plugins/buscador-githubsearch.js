cafafe7187e5d0f3a92ae2e42845722f097cbfc7
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-08-20 21:22:33 -0400

    Actualizar buscador-githubsearch.js

--- plugins/buscador-githubsearch.js
+++ plugins/buscador-githubsearch.js
@@ -1,6 +1,6 @@
 import fetch from 'node-fetch'
 const handler = async (m, {conn, text, usedPrefix, command}) => {
-if (!text) return conn.reply(m.chat, `🚩 *Ingrese el nombre de un repositorio de github*\n\nEjemplo, ${usedPrefix + command} Ai-Yaemori`, m, rcanal)
+if (!text) return conn.reply(m.chat, `🤍 *Ingrese el nombre de un repositorio de github*\n\nEjemplo, ${usedPrefix + command} Ai-Yaemori`, m, rcanal)
 try {
 await m.react(rwait)
 const res = await fetch(global.API('https://api.github.com', '/search/repositories', {
