9a68e9bdaed44a6bc827d0495f748a7adb794565
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-08-20 21:24:57 -0400

    Actualizar rpg-crimen.js

--- plugins/rpg-crimen.js
+++ plugins/rpg-crimen.js
@@ -8,40 +8,40 @@ let senderName = conn.getName(senderId)
 let tiempo = 5 * 60
 if (cooldowns[m.sender] && Date.now() - cooldowns[m.sender] < tiempo * 1000) {
 let tiempo2 = segundosAHMS(Math.ceil((cooldowns[m.sender] + tiempo * 1000 - Date.now()) / 1000))
-m.reply(`🍟 Ya has cometido un Crimen recientemente, espera ⏱️ *${tiempo2}* para cometer tu próximo Crimen y evitar ser atrapado.`)
+m.reply(`🤍 Ya has cometido un Crimen recientemente, espera ⏱️ *${tiempo2}* para cometer tu próximo Crimen y evitar ser atrapado.`)
 return
 }
 cooldowns[m.sender] = Date.now()
-let senderEstrellas = users[senderId].estrellas || 0
+let senderCorazones = users[senderId].estrellas || 0
 let randomUserId = Object.keys(users)[Math.floor(Math.random() * Object.keys(users).length)]
 while (randomUserId === senderId) {
 randomUserId = Object.keys(users)[Math.floor(Math.random() * Object.keys(users).length)]}
-let randomUserEstrellas = users[randomUserId].estrellas || 0
+let randomUserCorazones = users[randomUserId].corazones || 0
 let minAmount = 15
 let maxAmount = 50
 let amountTaken = Math.floor(Math.random() * (maxAmount - minAmount + 1)) + minAmount
 let randomOption = Math.floor(Math.random() * 3)
 switch (randomOption) {
 case 0:
-users[senderId].estrellas += amountTaken
-users[randomUserId].estrellas -= amountTaken
+users[senderId].corazones += amountTaken
+users[randomUserId].corazones -= amountTaken
 conn.sendMessage(m.chat, {
-text: `🚩¡Lograste cometer tu crimen con exito!, acabas de robar *${amountTaken} ⭐ Estrellas* a @${randomUserId.split("@")[0]}\n\nSe suman *+${amountTaken} ⭐ Estrellas* a ${senderName}.`,
+text: `🤍¡Lograste cometer tu crimen con exito!, acabas de robar *${amountTaken} 🤍 Corazones* a @${randomUserId.split("@")[0]}\n\nSe suman *+${amountTaken} 🤍 Corazones* a ${senderName}.`,
 contextInfo: { 
 mentionedJid: [randomUserId],
 }}, { quoted: m })
 break
 case 1:
-let amountSubtracted = Math.min(Math.floor(Math.random() * (senderEstrellas - minAmount + 1)) + minAmount, maxAmount)
-users[senderId].estrellas -= amountSubtracted
-conn.reply(m.chat, `🚩 No fuiste cuidadoso y te atraparon mientras cometias tu cirme, se restaron *-${amountSubtracted} ⭐ Estrellas* a ${senderName}.`, m, rcanal)
+let amountSubtracted = Math.min(Math.floor(Math.random() * (senderCorazones - minAmount + 1)) + minAmount, maxAmount)
+users[senderId].corazones -= amountSubtracted
+conn.reply(m.chat, `🤍 No fuiste cuidadoso y te atraparon mientras cometias tu cirme, se restaron *-${amountSubtracted} 🤍 Corazones* a ${senderName}.`, m, rcanal)
 break
 case 2:
-let smallAmountTaken = Math.min(Math.floor(Math.random() * (randomUserEstrellas / 2 - minAmount + 1)) + minAmount, maxAmount)
-users[senderId].estrellas += smallAmountTaken
-users[randomUserId].estrellas -= smallAmountTaken
+let smallAmountTaken = Math.min(Math.floor(Math.random() * (randomUserCorazones / 2 - minAmount + 1)) + minAmount, maxAmount)
+users[senderId].corazones += smallAmountTaken
+users[randomUserId].corazones -= smallAmountTaken
 conn.sendMessage(m.chat, {
-text: `🚩 Lograste cometer tu crimen con exito, pero te descubrieron y solo lograste tomar *${smallAmountTaken} ⭐ Estrellas* de @${randomUserId.split("@")[0]}\n\nSe suman *+${smallAmountTaken} ⭐ Estrellas* a ${senderName}.`,
+text: `🤍 Lograste cometer tu crimen con exito, pero te descubrieron y solo lograste tomar *${smallAmountTaken} 🤍 Corazones* de @${randomUserId.split("@")[0]}\n\nSe suman *+${smallAmountTaken} 🤍 Corazones* a ${senderName}.`,
 contextInfo: { 
 mentionedJid: [randomUserId],
 }}, { quoted: m })
