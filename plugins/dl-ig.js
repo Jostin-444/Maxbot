677093cae53706f37b1e31b992a98e40fc9a73f2
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-08-20 22:43:23 -0400

    Actualizar dl-ig.js

--- plugins/dl-ig.js
+++ plugins/dl-ig.js
@@ -22,6 +22,7 @@ let handler = async (m, { args, conn }) => {
     }
 }
 
+handler.corazones = 3
 handler.command = ['ig', 'igdl', 'instagram'];
 handler.tags = ['dl'];
 handler.help = ['ig <link>'];
