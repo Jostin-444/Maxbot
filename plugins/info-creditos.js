855ac65a0093eb528cb284b459fd6b3ea084624a
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-08-20 21:20:22 -0400

    Actualizar info-creditos.js

--- plugins/info-creditos.js
+++ plugins/info-creditos.js
@@ -1,31 +1,31 @@
 let handler = async (m, { conn, command, usedPrefix }) => {
-let staff = `🚩 *EQUIPO DE AYUDANTES*
-💫 *Bot:* ${global.botname}
-✨️ *Versión:* ${global.vs}
+let staff = `☁️ *EQUIPO DE AYUDANTES*
+☕ *Bot:* ${global.botname}
+☕ *Versión:* ${global.vs}
 
-👑 *Propietario:*
+🤍 *Propietario:* 🤍
 
 • Angel-OFC
-🍟 *Rol:* Propietario
-🚩 *Número:* ${creador}
-✨️ *GitHub:* https://github.com/Angelito-OFC
+☁️ *Rol:* Propietario
+☁️ *Número:* ${creador}
+☁️ *GitHub:* https://github.com/Angelito-OFC
 
-🌸  *Colaboradores:*
+🤍 *Colaboradores:* 🤍
 
 • Maxz XB
-🍟 *Rol:* Colab
-🚩 *Número:* Wa.me/5215610314499
-✨️ *GitHub:* https://github.com/ArizzVal
+☁️ *Rol:* Colab
+☁️ *Número:* Wa.me/5215610314499
+☁️ *GitHub:* https://github.com/ArizzVal
 
 • Alermz XB
-🍟 *Rol:* Colab
-🚩 *Número:* Wa.me/573147616444
-✨️ *GitHub:* https://github.com/elrebelde21
+☁️ *Rol:* Colab
+☁️ *Número:* Wa.me/573147616444
+☁️ *GitHub:* https://github.com/elrebelde21
 
 • Sebas XB
-🍟 *Rol:* Colab
-🚩 *Número:* Wa.me/5214434703586
-✨️ *GitHub:* https://github.com/AzamiJs`
+☁️ *Rol:* Colab
+☁️ *Número:* Wa.me/5214434703586
+☁️ *GitHub:* https://github.com/AzamiJs`
 await conn.sendFile(m.chat, icons, 'yaemori.jpg', staff.trim(), fkontak, true, {
 contextInfo: {
 'forwardingScore': 200,
@@ -33,8 +33,8 @@ contextInfo: {
 externalAdReply: {
 showAdAttribution: true,
 renderLargerThumbnail: false,
-title: `🥷 Developers 👑`,
-body: `🚩 Staff Oficial`,
+title: `☁️ Colaboradores ☁️`,
+body: `🤍 Staff Oficial`,
 mediaType: 1,
 sourceUrl: redes,
 thumbnailUrl: icono
