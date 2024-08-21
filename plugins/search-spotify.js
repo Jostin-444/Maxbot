26ae0191fcb5b3e827cbe487e37120d8d0d47bc7
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-08-20 22:02:58 -0400

    Actualizar search-spotify.js

--- plugins/search-spotify.js
+++ plugins/search-spotify.js
@@ -100,7 +100,7 @@ m.react('✅️')
 } catch (error) {
 }}
 handler.help = ['spotify <cancion>']
-handler.corazones = 1
+handler.corazones = 3
 handler.register = true
 handler.tags = ['search','dl']
 handler.command = /^(spotify|music)$/i
