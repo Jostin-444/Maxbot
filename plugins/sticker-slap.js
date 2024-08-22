c2df339c786d989edb53f8970623d6cc292e025a
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-08-21 23:03:44 -0400

    Actualizar sticker-slap.js

--- plugins/sticker-slap.js
+++ plugins/sticker-slap.js
@@ -22,7 +22,7 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
 
 }
 handler.help = ['slap @tag']
-handler.tags = ['rnime']
+handler.tags = ['sticker']
 handler.command = /^(slap|bofetada)$/i
 handler.diamond = true
 handler.group = true
