1b1dca2dde050633c86853b9e05f0f676f0ece93
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-08-21 22:59:37 -0400

    Actualizar dl-gitclone.js

--- plugins/dl-gitclone.js
+++ plugins/dl-gitclone.js
@@ -46,7 +46,7 @@ await m.react(error)
   }
 }
 handler.help = ['gitclone *<url git>*']
-handler.tags = ['descargas']
+handler.tags = ['dl']
 handler.command = /^(gitclone)$/i
 handler.register = true 
 //handler.star = 1
