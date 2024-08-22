a29a42bb22e583ec718b8b07a85a1ecf95957e14
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-08-21 23:18:16 -0400

    Actualizar owner-reporte.js

--- plugins/owner-reporte.js
+++ plugins/owner-reporte.js
@@ -1,13 +1,17 @@
-let handler = async (m, { conn, text, usedPrefix, command }) => {
-    if (!text) throw '⚠ *_️Ingrese el error ue desea reportar._*'
-    if (text.length < 10) throw '⚠️ *_Especifique bien el error, mínimo 10 caracteres._*'
-    if (text.length > 1000) throw '⚠️ *_Máximo 1000 caracteres para enviar el error._*'
-    const teks = `╭───────────────────\n│⊷〘 *R E P O R T E* 🤍 〙⊷\n├───────────────────\n│⁖🧡꙰  *Cliente:*\n│✏️ Wa.me/${m.sender.split`@`[0]}\n│\n│⁖💚꙰  *Mensaje:*\n│📩 ${text}\n╰───────────────────`
-    await conn.reply(global.owner[0][0] + '@s.whatsapp.net', m.quoted ? teks + m.quoted.text : teks, m, { mentions: conn.parseMention(teks) })
-    m.reply('⚠️ *_El reporte se envío a mi creador, cualquier informe falso puede ocasionar baneo._*')
-}
-handler.help = ['reportar']
-handler.tags = ['info']
-handler.command = /^(reporte|report|reportar|bug|error)$/i
-
-export default handler
+const handler = async (m, {conn, text, usedPrefix, command}) => {
+  if (!text) throw `🐈‍⬛ 𝙄𝙣𝙜𝙧𝙚𝙨𝙚 𝙪𝙣 𝙧𝙚𝙥𝙤𝙧𝙩𝙚\n\n*𝙴𝙹𝙴𝙼𝙿𝙻𝙾:*\n*${usedPrefix + command} el comando ${usedPrefix}play no manda nada*`;
+  if (text.length < 10) throw `🐈‍⬛ 𝙀𝙡 𝙧𝙚𝙥𝙤𝙧𝙩𝙚 𝙙𝙚𝙗𝙚 𝙨𝙚𝙧 𝙙𝙚 𝙢𝙞𝙣𝙞𝙢𝙤 10 𝙘𝙖𝙧𝙖𝙘𝙩𝙚𝙧𝙚𝙨`;
+  if (text.length > 1000) throw `🐈‍⬛ 𝙀𝙡 𝙧𝙚𝙥𝙤𝙧𝙩𝙚 𝙙𝙚𝙗𝙚 𝙨𝙚𝙧 𝙙𝙚 𝙢𝙖𝙭𝙞𝙢𝙤 1000 𝙘𝙖𝙧𝙖𝙘𝙩𝙚𝙧𝙚𝙨`;
+  const teks = `
+𝗠𝗘𝗡𝗦𝗔𝗝𝗘:\n• ${text}\n\n•╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍•\n𝗔𝗧𝗧:
+• wa.me/${m.sender.split`@`[0]}\n𝘙𝘦𝘱𝘰𝘳𝘵𝘦 𝘥𝘦 𝘦𝘳𝘳𝘰𝘳𝘦𝘴...`;
+  conn.reply('59897246324@s.whatsapp.net', m.quoted ? teks + m.quoted.text : teks, null, {contextInfo: {mentionedJid: [m.sender]}});
+  conn.reply('59176181985@s.whatsapp.net', m.quoted ? teks + m.quoted.text : teks, null, {contextInfo: {mentionedJid: [m.sender]}});
+  m.reply(`-ˋˏ ༻🐈‍⬛ 𝙀́𝙓𝙄𝙏𝙊 🐈‍⬛༺ ˎˊ-\n\n𝘌𝘭 𝘳𝘦𝘱𝘰𝘳𝘵𝘦 𝘢 𝘴𝘪𝘥𝘰 𝘦𝘯𝘷𝘪𝘢𝘥𝘰 𝘢𝘮𝘪 𝘤𝘳𝘦𝘢𝘥𝘰𝘳. 
+𝘛𝘦𝘯𝘥𝘳𝘢 𝘶𝘯𝘢 𝘳𝘦𝘴𝘱𝘶𝘦𝘴𝘵𝘢 𝘱𝘳𝘰𝘯𝘵𝘰.
+𝘋𝘦 𝘴𝘦𝘳 𝘧𝘢𝘭𝘴𝘰 𝘴𝘦𝘳𝘢 𝘪𝘨𝘯𝘰𝘳𝘢𝘥𝘰 𝘴𝘶 𝘳𝘦𝘱𝘰𝘳𝘵𝘦.`);
+};
+handler.help = ['reporte', 'request'].map((v) => v + ' <teks>');
+handler.tags = ['info'];
+handler.command = /^(report|request|reporte|bugs|bug|report-owner|reportes)$/i;
+export default handler;
