cc20c7058a21304a632b7532ccff9e77c2d217ab
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-08-22 08:50:01 -0400

    Actualizar info-script.js

--- plugins/info-script.js
+++ plugins/info-script.js
@@ -2,24 +2,23 @@ import moment from 'moment-timezone'
 import fetch from 'node-fetch'
 
 let handler = async (m, { conn, args }) => {
-let res = await fetch('https://api.github.com/repos/Angelito-OFC/GenesisBot-MD')
+let res = await fetch('https://api.github.com/repos/DanielDiod/Sumi-Sakurasawa')
 let json = await res.json()
 try {
-let txt = `*乂  S C R I P T  -  M A I N  乂*\n\n`
-    txt += `✩  *Nombre* : ${json.name}\n`
-    txt += `✩  *Visitas* : ${json.watchers_count}\n`
-    txt += `✩  *Peso* : ${(json.size / 1024).toFixed(2)} MB\n`
-    txt += `✩  *Actualizado* : ${moment(json.updated_at).format('DD/MM/YY - HH:mm:ss')}\n`
-    txt += `✩  *Url* : ${json.html_url}\n`
-    txt += `✩  *Forks* : ${json.forks_count}\n`
-    txt += `✩  *Stars* : ${json.stargazers_count}\n\n`
-    txt += `> 🤍 *${packname}*`
+let txt = '`- 乂  S C R I P T  -  M A I N`\n\n'
+    txt += `        •   *Nombre* : ${json.name}\n`
+    txt += `        •   *Visitas* : ${json.watchers_count}\n`
+    txt += `        •   *Peso* : ${(json.size / 1024).toFixed(2)} MB\n`
+    txt += `        •   *Actualizado* : ${moment(json.updated_at).format('DD/MM/YY - HH:mm:ss')}\n`
+    txt += `        •   *Url* : ${json.html_url}\n`
+    txt += `        •   *Forks* : ${json.forks_count}\n`
+    txt += `        •   *Stars* : ${json.stargazers_count}\n\n`
+    txt += `> 🚩 *${textbot}*`
+let img = await (await fetch(`https://i.ibb.co/LQKxczm/file.jpg`)).buffer()
 
-let img = imagen1
-
-await conn.sendluffy(m.chat, packname, wm, txt, img, img, redes, fkontak)
+await conn.sendluffy(m.chat, botname, textbot, txt, img, img, canal, m)
 } catch {
-await m.react(error)
+await m.react('✖️')
 }}
 handler.help = ['script']
 handler.tags = ['main']
