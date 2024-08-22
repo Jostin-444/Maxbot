ccb8f06d67a4b2d5b147e3613174cfe9338a6356
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-08-21 23:01:36 -0400

    Actualizar gc-hidetag.js

--- plugins/gc-hidetag.js
+++ plugins/gc-hidetag.js
@@ -18,7 +18,7 @@ text || q.text, conn.user.jid, { mentions: users }
 await conn.relayMessage(m.chat, msg.message, { messageId: msg.key.id })
 }
 handler.help = ['hidetag']
-handler.tags = ['group']
+handler.tags = ['grupo']
 handler.command = ['n', 'notify'] 
 handler.group = true
 handler.admin = true
