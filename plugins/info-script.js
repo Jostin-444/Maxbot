f5e6e497e9e003eb0591e7fd625485e2f53d6901
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-08-21 20:55:34 -0400

    Actualizar info-script.js

--- plugins/info-script.js
+++ plugins/info-script.js
@@ -2,7 +2,7 @@ import moment from 'moment-timezone'
 import fetch from 'node-fetch'
 
 let handler = async (m, { conn, args }) => {
-let res = await fetch('https://api.github.com/repos/OfcDiego/Ai-Yaemori')
+let res = await fetch('https://api.github.com/repos/Angelito-OFC/GenesisBot-MD')
 let json = await res.json()
 try {
 let txt = `*乂  S C R I P T  -  M A I N  乂*\n\n`
