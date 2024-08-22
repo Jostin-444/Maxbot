175ad6f70aeb5017a25ff2dfd089f307e5acdae5
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-08-21 21:42:33 -0400

    Actualizar plugins

---
+++ plugins/fun-gay.js
@@ -0,0 +1,13 @@
+const handler = async (m, {conn}) => {
+  const vn = './src/mp3/gay2.mp3';
+  const who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender;
+m.react('🏳️‍🌈')
+  await conn.sendFile(m.chat, global.API('https://some-random-api.com', '/canvas/gay', {
+    avatar: await conn.profilePictureUrl(who, 'image').catch((_) => 'https://telegra.ph/file/24fa902ead26340f3df2c.png'),
+  }), 'error.png', '*🏳️‍🌈 MIREN A ESTE GAY*', m);
+  await conn.sendMessage(m.chat, {audio: {url: vn}, fileName: `error.mp3`, mimetype: 'audio/mpeg', ptt: true}, {quoted: m});
+};
+handler.help = ['gay'];
+handler.tags = ['game'];
+handler.command = /^(gay)$/i;
+export default handler;
