db4a7b5ab8352828a73a7efe2e32676e10f83719
Jostin-444 <gemersport7@gmail.com>
2024-08-25 18:59:19 -0600

    Actualizar game-casino.js

--- plugins/game-casino.js
+++ plugins/game-casino.js
@@ -21,7 +21,7 @@ return
 cooldowns[m.sender] = Date.now()
 count = count ? /all/i.test(count) ? Math.floor(global.db.data.users[m.sender].limit / buatall) : parseInt(count) : args[0] ? parseInt(args[0]) : 1
 count = Math.max(1, count)
-if (args.length < 1) return conn.reply(m.chat, '💙 Ingresa la cantidad de ' + `💙 *Corazones*` + ' que deseas aportar contra' + ` *GenesisBot*` + `\n\n` + '`Ejemplo:`\n' + `> *${usedPrefix + command}* 100`, m, rcanal)
+if (args.length < 1) return conn.reply(m.chat, '💙 Ingresa la cantidad de ' + `💙 *Corazones*` + ' que deseas aportar contra' + ` *MaxBot*` + `\n\n` + '`Ejemplo:`\n' + `> *${usedPrefix + command}* 100`, m, rcanal)
 if (user.corazones >= count * 1) {
 user.corazones -= count * 1
 if (Aku > Kamu) {
