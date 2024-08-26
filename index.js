36b8b6e9cda64a893e886b351bd1d059eee5442a
Jostin-444 <gemersport7@gmail.com>
2024-08-26 14:21:24 -0600

    Actualizar index.js

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ break;
 });
 p.on('exit', (_, code) => {
 isRunning = false;
-console.error('🤍 Error:\n', code);
+console.error('🩵 Error:\n', code);
 process.exit();
 if (code === 0) return;
 watchFile(args[0], () => {
@@ -68,7 +68,7 @@ p.emit('message', line.trim());
 }
 process.on('warning', (warning) => {
 if (warning.name === 'MaxListenersExceededWarning') {
-console.warn('🤍 Se excedió el límite de Listeners en:');
+console.warn('🩵 Se excedió el límite de Listeners en:');
 console.warn(warning.stack);
 }
 });
