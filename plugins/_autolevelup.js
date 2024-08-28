ba0b73a93e99389c82ee6cc7520c43abf292a0f3
Jostin-444 <gemersport7@gmail.com>
2024-08-27 20:54:56 -0600

    Actualizar _autolevelup.js

--- plugins/_autolevelup.js
+++ plugins/_autolevelup.js
@@ -11,6 +11,6 @@ let before = user.level * 1
 while (canLevelUp(user.level, user.exp, global.multiplier)) user.level++
  if (before !== user.level) {
 
-conn.reply(m.chat, `🤍 *SUBISTE DE NIVEL* 🤍\n\n☁️ *Nuevo Nivel:* ${user.level}\n☁️ *Nivel Anterior:* ${before}\n☁️ *Rango:* ${user.role}\n☁️ *Fecha:* ${new Date().toLocaleString('id-ID')}`.trim(),m, rcanal, )
+conn.reply(m.chat, `💙 *SUBISTE DE NIVEL* 💙\n\n☁️ *Nuevo Nivel:* ${user.level}\n☁️ *Nivel Anterior:* ${before}\n☁️ *Rango:* ${user.role}\n☁️ *Fecha:* ${new Date().toLocaleString('id-ID')}`.trim(),m, rcanal, )
     }
 }
