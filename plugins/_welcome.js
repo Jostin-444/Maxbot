5fde6ecfdc71433a0d6f1c539756f4782f8730b2
Jostin-444 <gemersport7@gmail.com>
2024-08-27 21:22:24 -0600

    Actualizar _welcome.js

--- plugins/_welcome.js
+++ plugins/_welcome.js
@@ -8,16 +8,16 @@ export async function before(m, {conn, participants, groupMetadata}) {
   let chat = global.db.data.chats[m.chat]
 
   if (chat.welcome && m.messageStubType == 27) {
-    let welcome = `┌─★ 𝐆𝐞𝐧𝐞𝐬𝐢𝐬𝐁𝐨𝐭-𝐌𝐃\n│「 Bienvenido 」\n└┬★ 「 @${m.messageStubParameters[0].split`@`[0]} 」\n   │💙  Bienvenido a\n   │❤️  ${groupMetadata.subject}\n   └───────────────┈ ⳹`
+    let welcome = `┌─★ 𝗠𝗮𝘅𝐁𝐨𝐭-𝐌𝐃\n│「 Bienvenido 」\n└┬★ 「 @${m.messageStubParameters[0].split`@`[0]} 」\n   │💙  Bienvenido a\n   │❤️  ${groupMetadata.subject}\n   └───────────────┈ ⳹`
 await conn.sendLuffy(m.chat, packname, textbot, welcome, img, img, canal, estilo)
   }
 
   if (chat.welcome && m.messageStubType == 28) {
-    let bye = `┌─★ 𝐆𝐞𝐧𝐞𝐬𝐢𝐬𝐁𝐨𝐭-𝐌𝐃\n│「 ADIOS 👋 」\n└┬★ 「 @${m.messageStubParameters[0].split`@`[0]} 」\n   │💙  Se fue\n   │🩵 Jamás te quisimos aquí\n   └───────────────┈ ⳹`
+    let bye = `┌─★ 𝗠𝗮𝘅𝐁𝐨𝐭-𝐌𝐃\n│「 ADIOS 👋 」\n└┬★ 「 @${m.messageStubParameters[0].split`@`[0]} 」\n   │💙  Se fue\n   │🩵 Jamás te quisimos aquí\n   └───────────────┈ ⳹`
 await conn.sendLuffy(m.chat, packname, textbot, bye, img, img, canal, estilo)
   }
 
   if (chat.welcome && m.messageStubType == 32) {
-    let kick = `┌─★ 𝐆𝐞𝐧𝐞𝐬𝐢𝐬𝐁𝐨𝐭-𝐌𝐃\n│「 ADIOS 👋 」\n└┬★ 「 @${m.messageStubParameters[0].split`@`[0]} 」\n   │🩵 Se fue\n   │❤️ Jamás te quisimos aquí\n   └───────────────┈ ⳹`
+    let kick = `┌─★ 𝗠𝘅𝐁𝐨𝐭-𝐌𝐃\n│「 ADIOS 👋 」\n└┬★ 「 @${m.messageStubParameters[0].split`@`[0]} 」\n   │🩵 Se fue\n   │❤️ Jamás te quisimos aquí\n   └───────────────┈ ⳹`
 await conn.sendLuffy(m.chat, packname, textbot, kick, img, img, canal, estilo)
 }}
