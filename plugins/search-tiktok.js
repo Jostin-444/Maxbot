f3bc4255f9b5213e67ec2cea12fa9296ea944eac
Jostin-444 <gemersport7@gmail.com>
2024-08-27 20:55:49 -0600

    Actualizar search-tiktok.js

--- plugins/search-tiktok.js
+++ plugins/search-tiktok.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 const {proto, generateWAMessageFromContent, prepareWAMessageMedia, generateWAMessageContent, getDevice} = (await import("@whiskeysockets/baileys")).default
 
 let handler = async (message, { conn, text, usedPrefix, command }) => {
-if (!text) return conn.reply(message.chat, '🤍 *¿Que quieres buscar en tiktok?*', message, rcanal)
+if (!text) return conn.reply(message.chat, '💙 *¿Que quieres buscar en tiktok?*', message, rcanal)
 async function createVideoMessage(url) {
 const { videoMessage } = await generateWAMessageContent({ video: { url } }, { upload: conn.waUploadToServer })
 return videoMessage
@@ -38,7 +38,7 @@ deviceListMetadata: {},
 deviceListMetadataVersion: 2
 },
 interactiveMessage: proto.Message.InteractiveMessage.fromObject({
-body: proto.Message.InteractiveMessage.Body.create({ text: '🤍 *Resultado de:* ' + text }),
+body: proto.Message.InteractiveMessage.Body.create({ text: '💙 *Resultado de:* ' + text }),
 footer: proto.Message.InteractiveMessage.Footer.create({ text: '🔎 Tiktok - Busquedas' }),
 header: proto.Message.InteractiveMessage.Header.create({ hasMediaAttachment: false }),
 carouselMessage: proto.Message.InteractiveMessage.CarouselMessage.fromObject({ cards: [...results] })})}}
