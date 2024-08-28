4f6ff8024496295592ee8d0956501c38663eddfb
Jostin-444 <gemersport7@gmail.com>
2024-08-27 20:48:53 -0600

    Actualizar rpg-cartera.js

--- plugins/rpg-cartera.js
+++ plugins/rpg-cartera.js
@@ -3,7 +3,7 @@ let who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : m
 if (who == conn.user.jid) return error 
 if (!(who in global.db.data.users)) return conn.reply(m.chat, '🍭 El usuario no se encuentra en mi base de Datos.', m, fake)
 let user = global.db.data.users[who]
-await m.reply(`${who == m.sender ? `Tienes *${user.corazones} 🤍 Corazones* en tu Cartera` : `El usuario @${who.split('@')[0]} tiene *${user.corazones} 🤍 Corazones* en su Cartera`}. `, null, { mentions: [who] })}
+await m.reply(`${who == m.sender ? `Tienes *${user.corazones} 💙 Corazones* en tu Cartera` : `El usuario @${who.split('@')[0]} tiene *${user.corazones} 💙 Corazones* en su Cartera`}. `, null, { mentions: [who] })}
 
 handler.help = ['estrellas']
 handler.tags = ['rpg']
