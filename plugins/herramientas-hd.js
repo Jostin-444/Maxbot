5acaac6af95ffd8e355115a3e00cf1b602b90423
🌹፝⃟༘┼ ⃕⏤͟͟͞͞=- ꦿ⃟ۜMxz=𒆜.⋕ <Roldanrojas801@gmail.com>
2024-08-29 14:07:19 -0300

    Actualizar herramientas-hd.js

--- plugins/herramientas-hd.js
+++ plugins/herramientas-hd.js
@@ -4,48 +4,42 @@ const handler = async (m, {conn, usedPrefix, command}) => {
  try {    
   let q = m.quoted ? m.quoted : m;
   let mime = (q.msg || q).mimetype || q.mediaType || "";
-  if (!mime) return m.reply(`💙 Envie una imagen o responda a la imagen utilizando el comando: ${usedPrefix + command}`);
-  if (!/image\/(jpe?g|png)/.test(mime)) return m.reply(`💙 El formato del archivo (${mime}) no es compatible, envía o responda a una imagen`);
-  conn.reply(m.chat, '💙 Mejorando la calidad de la imagen....', m, {
-  contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, showAdAttribution: true,
-  title: packname,
-  body: wm,
-  previewType: 0, thumbnail: icons,
-  sourceUrl: channel }}})
+  if (!mime) throw `᥀·࣭࣪̇˖⚙️◗ 𝙍𝙚𝙨𝙥𝙤𝙣𝙙𝙖 𝙖 𝙪𝙣𝙖 𝙞𝙢𝙖𝙜𝙚𝙣 𝙥𝙖𝙧𝙖 𝙪𝙨𝙖𝙧 𝙚𝙨𝙩𝙚 𝙘𝙤𝙢𝙖𝙣𝙙𝙤.`;
+  if (!/image\/(jpe?g|png)/.test(mime)) throw `᥀·࣭࣪̇˖🚫◗ 𝙁𝙤𝙧𝙢𝙖𝙩𝙤 𝙚𝙧𝙧𝙤𝙣𝙚𝙤, 𝙧𝙚𝙨𝙥𝙤𝙣𝙙𝙖 𝙖 𝙪𝙣𝙖 𝙞𝙢𝙖𝙜𝙚𝙣 𝙥𝙖𝙧𝙖 𝙪𝙨𝙖𝙧 𝙚𝙨𝙩𝙚 𝙘𝙤𝙢𝙖𝙣𝙙𝙤.`;
+  m.reply('᥀·࣭࣪̇˖♻️◗ 𝘼𝙪𝙢𝙚𝙣𝙩𝙖𝙣𝙙𝙤 𝙘𝙖𝙡𝙞𝙙𝙖𝙙 𝙙𝙚 𝙡𝙖 𝙞𝙢𝙖𝙜𝙚𝙣...');
   let img = await q.download?.();
   let pr = await remini(img, "enhance");
-  conn.sendMessage(m.chat, {image: pr}, {quoted: fkontak});
+  conn.sendMessage(m.chat, {image: pr}, {quoted: m});
  } catch {
- return m.reply("💙 Ocurrió un error");
+  throw `᥀·࣭࣪̇˖⛔◗ 𝙊𝙘𝙪𝙧𝙧𝙞𝙤 𝙪𝙣 𝙚𝙧𝙧𝙤𝙧 𝙞𝙣𝙚𝙨𝙥𝙚𝙧𝙖𝙙𝙤 𝙖𝙡 𝙘𝙤𝙣𝙫𝙚𝙧𝙩𝙞𝙧 𝙡𝙖 𝙞𝙢𝙖𝙜𝙚𝙣 𝙚𝙣 𝙃𝘿, 𝙞𝙣𝙩𝙚𝙣𝙩𝙖𝙡𝙤 𝙙𝙚 𝙣𝙪𝙚𝙫𝙤.`;
  }
 };
-handler.help = ["remini", "hd", "enhance"];
-handler.tags = ["fun"];
-handler.command = ["remini", "hd", "enhance"];
+handler.help = ["remini", "hd"];
+handler.tags = ["convertir"];
+handler.command = ["remini", "hd"];
 export default handler;
-
 async function remini(imageData, operation) {
-  return new Promise(async (resolve, reject) => {
-    const availableOperations = ["enhance", "recolor", "dehaze"];
-    if (availableOperations.includes(operation)) {
-      operation = operation;
-    } else {
-      operation = availableOperations[0];
-    }
-    const baseUrl = "https://inferenceengine.vyro.ai/" + operation + ".vyro";
-    const formData = new FormData();
-    formData.append("image", Buffer.from(imageData), {filename: "enhance_image_body.jpg", contentType: "image/jpeg"});
-    formData.append("model_version", 1, {"Content-Transfer-Encoding": "binary", contentType: "multipart/form-data; charset=utf-8"});
-    formData.submit({url: baseUrl, host: "inferenceengine.vyro.ai", path: "/" + operation, protocol: "https:", headers: {"User-Agent": "okhttp/4.9.3", Connection: "Keep-Alive", "Accept-Encoding": "gzip"}},
-      function (err, res) {
-        if (err) reject(err);
-        const chunks = [];
-        res.on("data", function (chunk) {chunks.push(chunk)});
-        res.on("end", function () {resolve(Buffer.concat(chunks))});
-        res.on("error", function (err) {
-        reject(err);
-        });
-      },
-    );
-  });
+return new Promise(async (resolve, reject) => {
+const availableOperations = ["enhance", "recolor", "dehaze"];
+if (availableOperations.includes(operation)) {
+operation = operation;
+} else {
+operation = availableOperations[0];
+}
+const baseUrl = "https://inferenceengine.vyro.ai/" + operation + ".vyro";
+const formData = new FormData();
+formData.append("image", Buffer.from(imageData), {filename: "enhance_image_body.jpg", contentType: "image/jpeg"});
+formData.append("model_version", 1, {"Content-Transfer-Encoding": "binary", contentType: "multipart/form-data; charset=utf-8"});
+formData.submit({url: baseUrl, host: "inferenceengine.vyro.ai", path: "/" + operation, protocol: "https:", headers: {"User-Agent": "okhttp/4.9.3", Connection: "Keep-Alive", "Accept-Encoding": "gzip"}},
+function (err, res) {
+if (err) reject(err);
+const chunks = [];
+res.on("data", function (chunk) {chunks.push(chunk)});
+res.on("end", function () {resolve(Buffer.concat(chunks))});
+res.on("error", function (err) {
+reject(err);
+});
+},
+);
+});
 }
