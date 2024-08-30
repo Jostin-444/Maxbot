87efa9ffc65a562cdd01714ccc2d57e97d563063
Jostin-444 <gemersport7@gmail.com>
2024-08-29 23:51:26 -0600

    Actualizar convertidor-toibb.js

--- plugins/convertidor-toibb.js
+++ plugins/convertidor-toibb.js
@@ -33,7 +33,7 @@ let handler = async (m, { conn }) => {
         txt += `» File* : ${q.filename || 'x.jpg'}\n`
         txt += `» Extension* : ${api.data.data.image.extension}\n`
         txt += `» Delete* : ${api.data.data.delete_url}\n\n`
-        txt += `© By: Genesis`
+        txt += `© By: Max`
     await conn.sendFile(m.chat, api.data.data.url, 'ibb.jpg', txt, m, null, fwc)
     await m.react('✅')
   } else {
