0d33bc8d8c7f56b4173406b852e15e6a8fa38b91
Jostin-444 <gemersport7@gmail.com>
2024-09-06 16:18:07 -0600

    Actualizar menu-juegos.js

--- plugins/menu-juegos.js
+++ plugins/menu-juegos.js
@@ -10,7 +10,7 @@ const { generateWAMessageFromContent, proto } = (await import('@whiskeysockets/b
 const defaultMenu = {
   before: `“𝐇𝐨𝐥𝐚 *%name* 𝐒𝐨𝐲 𝐌𝐚𝐱-𝐁𝐨𝐭"
 
-✧ ▬▭▬▭▬ ✦✧✦ ▬▭▬▭▬ ✧ 
+◆ ▬▬▬▬▬▬ ❴✪❵ ▬▬▬▬▬▬ ◆
 
  '︶꒦꒷♡꒷꒦︶.
 ┊✶ 𝐂𝐥𝐢𝐞𝐧𝐭𝐞: %name
