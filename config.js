ce5757d13fafc4ffb3df51b342a53bb4b3a9953e
Jostin-444 <gemersport7@gmail.com>
2024-09-06 21:15:27 -0600

    Actualizar config.js

--- config.js
+++ config.js
@@ -56,8 +56,8 @@ global.estilox = { key: {  fromMe: false, participant: `0@s.whatsapp.net`, ...(f
 
 //• ↳ ◜𝑳𝑰𝑵𝑲𝑺◞ • 🌿
 global.id_canal = '120363310471144301@newsletter' //-ID de canal de WhatsApp
-global.canal = 'https://whatsapp.com/channel/0029VaJxgcB0bIdvuOwKTM2Y' //CANAL GENESIS
-global.canal2 = 'https://whatsapp.com/channel/0029Vai2U3MAu3aRB0NYFT3h' //CANAL MAX-BOT
+global.canal = 'https://whatsapp.com/channel/0029Vai2U3MAu3aRB0NYFT3h' //CANAL GENESIS
+global.canal2 = 'https://whatsapp.com/channel/0029VafjvaaEgGfOe5KdmX1n' //CANAL MAX-BOT
 global.bgp = 'https://chat.whatsapp.com/KC6Ye8u87vE0E2Lrm5cZfk' //GRUPO DE AVISOS
 global.bgp2 = 'https://chat.whatsapp.com/CRIhRGP6fTp78OGWCUKQVm' //GRUPO GLOBAL 1
 global.bgp3 = 'https://chat.whatsapp.com/C2at9CUaxBn80MFj2UOfU9' //GRUPO GLOBAL 2
