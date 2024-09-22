2fadb16f95c873ca9cf147a3691be765f66258cc
Jostin-444 <gemersport7@gmail.com>
2024-09-22 15:12:28 -0600

    Actualizar herramientas-hd.js

--- plugins/herramientas-hd.js
+++ plugins/herramientas-hd.js
@@ -1,53 +1,52 @@
-import FormData from "form-data";
-import Jimp from "jimp";
+import fs from "fs"
+import fetch from "node-fetch"
+import FormData from "form-data"
 
+let handler = async m => {
+  try {
+    const q = m.quoted || m
+    const mime = q.mediaType || ""    
+    if (!/image|video|audio|sticker|document/.test(mime)) 
+      throw "¡No se proporcionan medios!"
+    const media = await q.download(true)
+    const fileSizeInBytes = fs.statSync(media).size    
+    if (fileSizeInBytes === 0) {
+      await m.reply("archivo vacio")
+      await fs.promises.unlink(media)
+      return
+    }   
+    if (fileSizeInBytes > 1073741824) {
+      await m.reply("El archivo es demasiado grande, el tamaño máximo es 1 GB")
+      await fs.promises.unlink(media)
+      return
+    }    
+    const { files } = await uploadUguu(media)
+    const caption = `*Link:*\n${files[0]?.url}`
+    await m.reply(caption)
+  } catch (e) {
+    await m.reply(`${e}`)
+  }
+}
 
-const handler = async (m, {conn, usedPrefix, command}) => {
-  const datas = global
-  const idioma = datas.db.data.users[m.sender].language || global.defaultLenguaje
-  const _translate = JSON.parse(fs.readFileSync(`./src/languages/${idioma}.json`))
-  const tradutor = _translate.plugins.herramientas_hd
+handler.help = ['tourl']
+handler.tags = ['convertir']
+handler.command = /^(tourl|upload)$/i
+export default handler
 
- try {    
-  let q = m.quoted ? m.quoted : m;
-  let mime = (q.msg || q).mimetype || q.mediaType || "";
-  if (!mime) throw `${tradutor.texto1} ${usedPrefix + command}*`;
-  if (!/image\/(jpe?g|png)/.test(mime)) throw `${tradutor.texto2[0]} (${mime}) ${tradutor.texto2[1]}`;
-  m.reply(tradutor.texto3);
-  let img = await q.download?.();
-  let pr = await remini(img, "enhance");
-  conn.sendMessage(m.chat, {image: pr}, {quoted: m});
- } catch {
-  throw tradutor.texto4;
- }
-};
-handler.help = ["remini", "hd", "enhance"];
-handler.tags = ["ai", "tools"];
-handler.command = ["remini", "hd", "enhance"];
-export default handler;
-
-async function remini(imageData, operation) {
-  return new Promise(async (resolve, reject) => {
-    const availableOperations = ["enhance", "recolor", "dehaze"];
-    if (availableOperations.includes(operation)) {
-      operation = operation;
-    } else {
-      operation = availableOperations[0];
-    }
-    const baseUrl = "https://inferenceengine.vyro.ai/" + operation + ".vyro";
-    const formData = new FormData();
-    formData.append("image", Buffer.from(imageData), {filename: "enhance_image_body.jpg", contentType: "image/jpeg"});
-    formData.append("model_version", 1, {"Content-Transfer-Encoding": "binary", contentType: "multipart/form-data; charset=utf-8"});
-    formData.submit({url: baseUrl, host: "inferenceengine.vyro.ai", path: "/" + operation, protocol: "https:", headers: {"User-Agent": "okhttp/4.9.3", Connection: "Keep-Alive", "Accept-Encoding": "gzip"}},
-      function (err, res) {
-        if (err) reject(err);
-        const chunks = [];
-        res.on("data", function (chunk) {chunks.push(chunk)});
-        res.on("end", function () {resolve(Buffer.concat(chunks))});
-        res.on("error", function (err) {
-        reject(err);
-        });
-      },
-    );
-  });
+async function uploadUguu(path) {
+  try {
+    const form = new FormData()
+    form.append("files[]", fs.createReadStream(path))   
+    const res = await fetch("https://uguu.se/upload.php", {
+      method: "POST",
+      headers: form.getHeaders(),
+      body: form
+    })    
+    const json = await res.json()
+    await fs.promises.unlink(path)   
+    return json
+  } catch (e) {
+    await fs.promises.unlink(path)
+    throw "Upload failed"
+  }
 }
