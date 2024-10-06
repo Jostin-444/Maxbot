b76a12754d8cf4ddeac284b5b3babe87634da612
Jostin-444 <gemersport7@gmail.com>
2024-10-06 13:02:11 -0600

    Actualizar plugins

---
+++ plugins/redes-delbot.js
@@ -0,0 +1,10 @@
+let handler = async (m) => {
+await m.reply("Estas son las redes DE MaxBot
+ig = https://www.instagram.com/jostin_max.bot?igsh=MW1jcDBtMWN2dm9qeQ==
+Github = https://github.com/Jostin-444/Maxbot
+*NOS AYUDARÍAS MUCHO DEJANDO TU 🌟")
+m.react("🤴")
+} 
+handler.customPrefix = ['max', 'redes'']
+handler.command = New RegExp
+export default handler
