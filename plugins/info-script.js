5975ecf19e3b295f10c727027c3425736aeb34e3
Jostin-444 <gemersport7@gmail.com>
2024-10-06 12:43:41 -0600

    Actualizar info-script.js

--- plugins/info-script.js
+++ plugins/info-script.js
@@ -1,26 +1,34 @@
-import moment from 'moment-timezone'
-import fetch from 'node-fetch'
+import moment from 'moment-timezone';
+import fetch from 'node-fetch';
 
 let handler = async (m, { conn, args }) => {
-let res = await fetch('https://api.github.com/repos/DanielDiod/Sumi-Sakurasawa')
-let json = await res.json()
-try {
-let txt = '`- 乂  S C R I P T  -  M A I N`\n\n'
-    txt += `        •   *Nombre* : ${json.name}\n`
-    txt += `        •   *Visitas* : ${json.watchers_count}\n`
-    txt += `        •   *Peso* : ${(json.size / 1024).toFixed(2)} MB\n`
-    txt += `        •   *Actualizado* : ${moment(json.updated_at).format('DD/MM/YY - HH:mm:ss')}\n`
-    txt += `        •   *Url* : ${json.html_url}\n`
-    txt += `        •   *Forks* : ${json.forks_count}\n`
-    txt += `        •   *Stars* : ${json.stargazers_count}\n\n`
-let img = await (await fetch(`https://i.ibb.co/HKS2v23/file.jpg`)).buffer()
+  try {
+    let res = await fetch('https://api.github.com/repos/David-Chian/Megumin-Bot-MD');
+    if (!res.ok) throw new Error('Error al obtener datos del repositorio');
+    let json = await res.json();
 
-await conn.sendluffy(m.chat, botname, textbot, txt, img, img, canal, m)
-} catch {
-await m.react('💙')
-}}
-handler.help = ['script']
-handler.tags = ['main']
-handler.command = ['script', 'sc']
-handler.register = true 
-export default handler
+    let txt = `*乂  S C R I P T  -  M A I N  乂*\n\n`;
+    txt += `✩  *Nombre* : ${json.name}\n`;
+    txt += `✩  *Visitas* : ${json.watchers_count}\n`;
+    txt += `✩  *Peso* : ${(json.size / 1024).toFixed(2)} MB\n`;
+    txt += `✩  *Actualizado* : ${moment(json.updated_at).format('DD/MM/YY - HH:mm:ss')}\n`;
+    txt += `✩  *Url* : ${json.html_url}\n`;
+    txt += `✩  *Forks* : ${json.forks_count}\n`;
+    txt += `✩  *Stars* : ${json.stargazers_count}\n\n`;
+    txt += `💥 *${packname}*`;
+
+    let img = imagen1;
+
+    await conn.sendMini(m.chat, packname, wm, txt, img, img, redes, fkontak);
+  } catch (error) {
+    console.error(error);
+    await m.react(error);  // Reacciona con un emoji de error si ocurre un problema
+  }
+};
+
+handler.help = ['script'];
+handler.tags = ['main'];
+handler.command = ['script', 'sc'];
+handler.register = true;
+
+export default handler;
