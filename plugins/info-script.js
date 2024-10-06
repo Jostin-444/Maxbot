813eff276c8d184dff3551cea60c6f21e10f8ff9
Jostin-444 <gemersport7@gmail.com>
2024-10-06 12:45:28 -0600

    Actualizar info-script.js

--- plugins/info-script.js
+++ plugins/info-script.js
@@ -1,34 +1,28 @@
-import moment from 'moment-timezone';
-import fetch from 'node-fetch';
+let handler = async (m, { conn, command, usedPrefix }) => {
+let pp = 'https://iili.io/ds8mmFI.md.png'
+m.react('🤍')
+let name = await conn.getName(m.sender)
+let _uptime = process.uptime() * 1000
+let _muptime
+if (process.send) { process.send('uptime')
+_muptime = await new Promise(resolve => { process.once('message', resolve) 
+setTimeout(resolve, 1000) }) * 1000}
+let uptime = clockString(_uptime)
+let estado = `\`乂 S C R I P T  -  B O T\`
 
-let handler = async (m, { conn, args }) => {
-  try {
-    let res = await fetch('https://api.github.com/repos/David-Chian/Megumin-Bot-MD');
-    if (!res.ok) throw new Error('Error al obtener datos del repositorio');
-    let json = await res.json();
+> 😼 *Te gusta nuestro Bot te invito a dejarnos una estrella a nuestro repositorio para seguir mejorando nuestro Bot* ☕
+`
+await conn.sendButton(m.chat, estado, '@usxr_angelito0', pp, [
+['DUEÑO 🤍', '.owner']], null, [['GIT HUB 🤍', `https://github.com/Angelito-OFC/GenesisBot-MD`]], estilo)
+}
+handler.help = ['script']
+handler.tags = ['info']
+handler.command = /^(sc|script|Repositorio?)$/i
 
-    let txt = `*乂  S C R I P T  -  M A I N  乂*\n\n`;
-    txt += `✩  *Nombre* : ${json.name}\n`;
-    txt += `✩  *Visitas* : ${json.watchers_count}\n`;
-    txt += `✩  *Peso* : ${(json.size / 1024).toFixed(2)} MB\n`;
-    txt += `✩  *Actualizado* : ${moment(json.updated_at).format('DD/MM/YY - HH:mm:ss')}\n`;
-    txt += `✩  *Url* : ${json.html_url}\n`;
-    txt += `✩  *Forks* : ${json.forks_count}\n`;
-    txt += `✩  *Stars* : ${json.stargazers_count}\n\n`;
-    txt += `💥 *${packname}*`;
+export default handler
 
-    let img = imagen1;
-
-    await conn.sendMini(m.chat, packname, wm, txt, img, img, redes, fkontak);
-  } catch (error) {
-    console.error(error);
-    await m.react(error);  // Reacciona con un emoji de error si ocurre un problema
-  }
-};
-
-handler.help = ['script'];
-handler.tags = ['main'];
-handler.command = ['script', 'sc'];
-handler.register = true;
-
-export default handler;
+function clockString(ms) {
+let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000)
+let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
+let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
+return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':')}
