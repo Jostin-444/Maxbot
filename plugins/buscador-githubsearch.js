90de43cfdf95ffa5a222030b6d5100d6daa4a1fb
Jostin-444 <gemersport7@gmail.com>
2024-10-06 12:41:12 -0600

    Actualizar buscador-githubsearch.js

--- plugins/buscador-githubsearch.js
+++ plugins/buscador-githubsearch.js
@@ -1,6 +1,6 @@
 import fetch from 'node-fetch'
 const handler = async (m, {conn, text, usedPrefix, command}) => {
-if (!text) return conn.reply(m.chat, `🩵 *Ingrese el nombre de un repositorio de github*\n\nEjemplo, ${usedPrefix + command} Ai-Yaemori`, m, rcanal)
+if (!text) return conn.reply(m.chat, `✍️ *Ingrese el nombre de un repositorio de github*\n\nEjemplo, ${usedPrefix + command} Yuki_Suou-Bot`, m, rcanal)
 try {
 await m.react(rwait)
 const res = await fetch(global.API('https://api.github.com', '/search/repositories', {
@@ -10,29 +10,29 @@ const json = await res.json()
 if (res.status !== 200) throw json
 let str = json.items.map((repo, index) => {
 return `
-☁️ *Resultado:* ${1 + index}
+⌛ *Resultado:* ${1 + index}
 🔗 *Enlace:* ${repo.html_url}
 👑 *Creador:* ${repo.owner.login}
-☁️ *Nombre:* ${repo.name}
+📃 *Nombre:* ${repo.name}
 🫂 *Creado:* ${formatDate(repo.created_at)}
 💥 *Actualizado:* ${formatDate(repo.updated_at)}
 👀 *Visitas:* ${repo.watchers}
 ✨️ *Bifurcado:* ${repo.forks}
 🌟 *Estrellas:* ${repo.stargazers_count}
 🍂 *Issues:* ${repo.open_issues}
-🍭 *Descripción:* ${repo.description ? `${repo.description}` : 'Sin Descripción'}
+♨️ *Descripción:* ${repo.description ? `${repo.description}` : 'Sin Descripción'}
 ⭐️ *Clone:* ${repo.clone_url}
 `.trim()}).join('\n\n─────────────────\n\n') 
 // await m.react(done)
 let img = await (await fetch(json.items[0].owner.avatar_url)).buffer()
-await conn.sendMini(m.chat, '🩵 G I T H U B - S E A R C H 🩵', dev, str, img, img, redes, estilo)
+await conn.sendMini(m.chat, '✨ G I T H U B - S E A R C H ✨', dev, str, img, img, redes, estilo)
 await m.react(done)
 } catch {
 await m.react(error)
-conn.reply(m.chat, '🩵 *No se encontró resultados de:* ' + text, m, fake)}}
+conn.reply(m.chat, '🚫 *No se encontró resultados de:* ' + text, m, fake)}}
 handler.help = ['githubsearch']
 handler.tags = ['buscador']
-handler.command = /^(githubsearch)$/i
+handler.command = ['githubsearch']
 
 handler.register = true
 
@@ -48,7 +48,7 @@ import fs from 'fs'
 
 var handler = async (m, { conn, text, usedPrefix, command }) => {
 
-if (!text) return conn.reply(m.chat, `🩵 *Ingrese el nombre de un repositorio de github*\n\nEjemplo, ${usedPrefix + command} Ai-Yaemori`, m, rcanal)
+if (!text) return conn.reply(m.chat, `✍️ *Ingrese el nombre de un repositorio de github*\n\nEjemplo, ${usedPrefix + command} Ai-Yaemori`, m, rcanal)
 
 try {
 
@@ -57,17 +57,17 @@ let json = await res.json()
 if (res.status !== 200) throw json
 let str = json.items.map((repo, index) => {
 return `
-☁️ *Resultado:* ${1 + index}
+⌛ *Resultado:* ${1 + index}
 🔗 *Enlace:* ${repo.html_url}
 👑 *Creador:* ${repo.owner.login}
-☁️ *Nombre:* ${repo.name}
+📃 *Nombre:* ${repo.name}
 🫂 *Creado:* ${formatDate(repo.created_at)}
 💥 *Actualizado:* ${formatDate(repo.updated_at)}
 👀 *Visitas:* ${repo.watchers}
 ✨️ *Bifurcado:* ${repo.forks}
 🌟 *Estrellas:* ${repo.stargazers_count}
 🍂 *Issues:* ${repo.open_issues}
-🍭 *Descripción:* ${repo.description ? `${repo.description}` : 'Sin Descripción'}
+♨️ *Descripción:* ${repo.description ? `${repo.description}` : 'Sin Descripción'}
 ⭐️ *Clone:* ${repo.clone_url}
 `.trim()}).join('\n\n─────────────────\n\n')
 
@@ -77,12 +77,12 @@ await conn.sendUrl(m.chat, str, m, { externalAdReply: { mediaType: 1, renderLarg
 })
 
 } catch {
-conn.reply(m.chat, '🩵 *Ocurrió un fallo*', m, fake)
+conn.reply(m.chat, '⚙️ *Ocurrió un fallo*', m, fake)
 }
 
 }
 handler.help = ['githubsearch']
-handler.tags = ['search']
+handler.tags = ['buscador']
 handler.command = /^(githubsearch)$/i
 
 handler.register = true
